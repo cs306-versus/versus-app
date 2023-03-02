2aaf59eefc58de3dfc81fc332f7d6c1c8311e11f
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-03-02 17:28:29 +0100

    Change default Activity + create sign-in Activity

---
+++ app/src/main/java/com/github/versus/EntryActivity.java
@@ -0,0 +1,15 @@
+package com.github.versus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO HR : Check if user is logged in
+        // TODO HR : If yes, switch to
+    }
+}
