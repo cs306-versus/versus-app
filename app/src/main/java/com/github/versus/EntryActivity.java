4d478ca4c891c8b9be374307da0a6f3109767663
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-03-02 18:04:46 +0100

    Draft of the Sign-In

--- app/src/main/java/com/github/versus/EntryActivity.java
+++ app/src/main/java/com/github/versus/EntryActivity.java
@@ -2,14 +2,33 @@ package com.github.versus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static java.util.Objects.*;
 
 public class EntryActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // TODO HR : Check if user is logged in
-        // TODO HR : If yes, switch to
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //Class<?> activity = isNull(mAuth.getCurrentUser()) ? SignInActivity.class : MainActivity.class;
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/github/versus/SignInActivity.java
+++ app/src/main/java/com/github/versus/SignInActivity.java
@@ -3,12 +3,65 @@ package com.github.versus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignIn";
+
+    private FirebaseAuth mAuth;
+
+    private Button signInButton;
+    private EditText mail;
+    private EditText pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        // Init all UI Components
+        init();
+        registerClickListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void init(){
+        mAuth = FirebaseAuth.getInstance();
+        signInButton = findViewById(R.id.sign_in);
+        mail = findViewById(R.id.mail);
+        pwd = findViewById(R.id.pwd);
+    }
+
+    private void registerClickListener(){
+        signInButton.setOnClickListener(view -> {
+            String m = mail.getText().toString();
+            String p = pwd.getText().toString();
+            mAuth.createUserWithEmailAndPassword(m, p)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    });
+        });
     }
 }
