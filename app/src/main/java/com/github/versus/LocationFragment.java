14836150b3e91359be9578f37a7935b8be2b4943
unknown <adam.mernissiarifi@epfl.ch>
2023-03-03 18:45:35 +0100

    Google Maps with fragment

--- app/src/main/java/com/github/versus/LocationFragment.java
+++ app/src/main/java/com/github/versus/LocationFragment.java
@@ -1,18 +1,87 @@
 package com.github.versus;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class LocationFragment  extends Fragment {
-    @Nullable
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap map;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_location,container,false);
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setZoomControlsEnabled(true);
+
+        LatLng epfl = new LatLng(46.520536, 6.568318);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(epfl, 15));
+        LatLng sat = new LatLng(46.520544, 6.567825);
+
+        map.addMarker(new MarkerOptions()
+                .position(epfl)
+                .title("EPFL "));
+        map.addMarker(new MarkerOptions()
+                .position(sat)
+                .title("Satellite "));
+
+        // [START_EXCLUDE silent]
+        map.moveCamera(CameraUpdateFactory.newLatLng(epfl));
+        map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                LatLng position = marker.getPosition();
+                String message = "Marker clicked at: " + position.latitude + ", " + position.longitude;
+                Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (map != null) {
+            getChildFragmentManager().beginTransaction().remove(getChildFragmentManager().findFragmentById(R.id.map)).commit();
+            map = null;
+        }
     }
 }
+
+
+
+
+
+
+
+
