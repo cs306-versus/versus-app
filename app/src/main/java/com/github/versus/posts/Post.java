501ec55893d4fac1cd9ff6fe7ef165379a75fd0a
ziazi <mehdi.ziazi@epfl.ch>
2023-03-08 02:24:34 +0100

    committing the implementation of the classes/interface

--- app/src/main/java/com/github/versus/posts/Post.java
+++ app/src/main/java/com/github/versus/posts/Post.java
@@ -1,2 +1,48 @@
-package com.github.versus.posts;public interface Post {
+package com.github.versus.posts;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Post in the Versus app
+ */
+public interface Post {
+
+    /**
+     *
+     * @return title of the post
+     */
+    public String getTitle();
+
+    /**
+     *
+     * @return date of the post
+     */
+    public Timestamp getDate();
+
+    /**
+     *
+     * @return The location of the game
+     */
+    public Location getLocation();
+
+    /**
+     /*TODO : Object needs to be changed to user once the interface is available
+     * @return the users who joined the post
+     */
+    public List<Object> getPlayers();
+
+
+    /**
+     *
+     * @return the limit of players that could join the post
+     */
+    public int getPlayerLimit();
+
+    /**
+     *
+     * @return all the attributes of the post in a map fashion
+     */
+    public Map<String, Object> getAllAttributes();
+
 }
--- app/src/main/java/com/github/versus/posts/Timestamp.java
+++ app/src/main/java/com/github/versus/posts/Timestamp.java
@@ -1,2 +1,97 @@
-package com.github.versus.posts;public class Timestamp {
-}
+package com.github.versus.posts;
+
+import java.net.MulticastSocket;
+import java.time.Month;
+import java.time.Year;
+import java.time.YearMonth;
+import java.util.Calendar;
+
+public class Timestamp {
+
+    private static final int CURR_YEAR = Calendar.getInstance().get(Calendar.YEAR);
+    private static final int MAX_YEAR = CURR_YEAR + 1;
+    private final int year;
+    private final Month month;
+    private final int day;
+
+    private final int hour;
+    private final int minutes;
+    private final int seconds;
+    private final Meridiem m;
+
+    public Timestamp(int year, Month month, int day, int hour, int minutes, Meridiem m) {
+        // TODO : enable assertions for these checks to happen
+        // acceptable time and date checks
+        assert( CURR_YEAR <= year && year <= MAX_YEAR );
+        assert( 1 <= day && day <= 31 );
+        assert( 1 <=  hour && hour <= 12 );
+        assert( 1 <=  minutes && minutes <= 60 );
+
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minutes = minutes;
+        this.seconds = 0;
+        this.m = m;
+    }
+
+
+    /**
+     *
+     * @return the day of the timestamp
+     */
+    public int getDay() {
+        return day;
+    }
+
+    /**
+     *
+     * @return the month of the timestamp
+     */
+    public Month getMonth() {
+        return month;
+    }
+
+    /**
+     *
+     * @return the year of the timestamp
+     */
+    public int getYear() {
+        return year;
+    }
+    /**
+     *
+     * @return the hour of the timestamp
+     */
+    public int getHour() {
+        return hour;
+    }
+    /**
+     *
+     * @return the minutes of the timestamp
+     */
+    public int getMinutes() {
+        return minutes;
+    }
+    /**
+     *
+     * @return the seconds of the timestamp
+     */
+    public int getSeconds() {
+        return seconds;
+    }
+    /**
+     *
+     * @return the meridiem of the timestamp
+     */
+    public Meridiem getM() {
+        return m;
+    }
+
+    /**
+     * AM-PM enum
+     */
+    enum Meridiem{
+        AM, PM;
+    }}
