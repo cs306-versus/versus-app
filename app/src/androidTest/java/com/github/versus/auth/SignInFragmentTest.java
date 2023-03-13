84c8b6ae381034d1afd6d35861480a091c5c0bfc
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-03-13 16:23:05 +0100

    Add SignInFragmentTest

---
+++ app/src/androidTest/java/com/github/versus/auth/SignInFragmentTest.java
@@ -0,0 +1,68 @@
+package com.github.versus.auth;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.mockito.Mockito.mock;
+
+import android.content.Context;
+import android.widget.Button;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.github.versus.R;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Objects;
+
+@RunWith(AndroidJUnit4.class)
+public class SignInFragmentTest {
+
+    @Rule
+    public ActivityScenarioRule<AuthActivity> scenario = new ActivityScenarioRule<>(AuthActivity.class);
+
+    private static final String test_account_mail = "demo-test@versus.ch";
+
+    private static final String test_account_pwd = "123456789";
+
+    @Before
+    public void setUp(){
+        FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(test_account_mail, test_account_pwd)
+                .addOnSuccessListener(res -> Objects.requireNonNull(res.getUser()).delete());
+        while(FirebaseAuth.getInstance().getCurrentUser() != null); // Wait for user to be deleted
+    }
+
+    @Test
+    public void testOnSuccessSignIn(){
+        onView(withId(R.id.auth_signin)).perform(click());
+        onView(withId(R.id.auth_signin_mail)).perform(replaceText(test_account_mail));
+        onView(withId(R.id.auth_signin_pwd)).perform(replaceText(test_account_pwd));
+        onView(withId(R.id.auth_signin_button)).perform(click());
+        while(FirebaseAuth.getInstance().getCurrentUser() == null); // Wait for login
+        Task<?> task = FirebaseAuth.getInstance().getCurrentUser().delete();
+        while(!task.isComplete()); // Wait for task to complete
+    }
+
+    @Test
+    public void testOnFailSignIn(){
+        onView(withId(R.id.auth_signin)).perform(click());
+        Task<?> task = FirebaseAuth.getInstance().createUserWithEmailAndPassword(test_account_mail, test_account_pwd);
+        while(!task.isComplete()); // Wait for task to complete
+        onView(withId(R.id.auth_signin_mail)).perform(replaceText(test_account_mail));
+        onView(withId(R.id.auth_signin_pwd)).perform(replaceText(test_account_pwd));
+        onView(withId(R.id.auth_signin_button)).perform(click());
+    }
+
+    // TODO HR : Add tests here
+
+}
