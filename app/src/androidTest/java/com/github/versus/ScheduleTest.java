3afcffeb5db5881a76bbd6b958e336a3758507de
unknown <aymane.lamyaghri@epfl.ch>
2023-03-15 19:09:17 +0100

    Added Tests for schedule buttons

---
+++ app/src/androidTest/java/com/github/versus/ScheduleTest.java
@@ -0,0 +1,48 @@
+package com.github.versus;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static java.util.Objects.requireNonNull;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.github.versus.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MailPasswordFragmentTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
+
+    private static final String test_account_mail = "demo@versus.ch";
+
+    private static final String test_account_pwd = "123456789";
+
+    @Test
+    public void testScheduleButtons(){
+        onView(withId(R.id.rectangle_18)).perform(click());
+        onView(withId(R.id.rectangle_17)).perform(click());
+        onView(withId(R.id.rectangle_3)).perform(click());
+        onView(withId(R.id.rectangle_14)).perform(click());
+        onView(withId(R.id.rectangle_18)).perform(click());
+        onView(withId(R.id.rectangle_18)).perform(click());
+        onView(withId(R.id.rectangle_18)).perform(click());
+        onView(withId(R.id.rectangle_18)).perform(click());
+
+        scenario.getScenario().onActivity(a -> requireNonNull(a.getPackageName()));
+    }
+
+
+
+}
