b1bb5fb5a9331b662e782c7c6b733da1e06fb6a4
unknown <aymane.lamyaghri@epfl.ch>
2023-03-15 18:34:42 +0100

    Added date change when switching days

--- app/src/main/java/com/github/versus/MyAdapter.java
+++ app/src/main/java/com/github/versus/MyAdapter.java
@@ -10,11 +10,11 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class MyAdapter extends ArrayAdapter<String> {
+public class MyAdapter extends ArrayAdapter<List<String>> {
     private Context context;
-    private List<String> books;
+    private List<List<String>> books;
 
-    public MyAdapter(Context context, List<String> books) {
+    public MyAdapter(Context context,List< List<String>> books) {
         super(context, R.layout.list_item, books);
         this.context = context;
         this.books = books;
@@ -24,11 +24,14 @@ public class MyAdapter extends ArrayAdapter<String> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.list_item, parent, false);
+        List<String> book = books.get(position);
+        ((TextView)view.findViewById(R.id.basketball_game)).setText(book.get(0));
+        ((TextView)view.findViewById(R.id.unil_sport_ek1)).setText(book.get(1));
+        ((TextView)view.findViewById(R.id._10_00_am)).setText(book.get(2));
 
         //TextView titleTextView = (TextView) view.findViewById(R.id.text_view_1);
         //TextView authorTextView = (TextView) view.findViewById(R.id.text_view_2);
 
-        String book = books.get(position);
       //  titleTextView.setText(book);
        // authorTextView.setText("this is text2");
 
--- app/src/main/java/com/github/versus/ScheduleFragment.java
+++ app/src/main/java/com/github/versus/ScheduleFragment.java
@@ -29,11 +29,20 @@ public class ScheduleFragment extends Fragment {
         String[] items = {"Item 1", "Item 2", "Item 3"};
 
         // Create an ArrayAdapter to populate the ListView
+        TextView dateText=view.findViewById(R.id._7_march___2023);
+        dateText.setText("12 March , 2023");
         List a =new ArrayList<String>();
-        a.add("haha");
-        a.add("hehe");
-       a.add("this");
-       MyAdapter  adapter = new MyAdapter(getContext(), a);
+        a.add("Football Game");
+        a.add("Unil-Sport");
+       a.add("10:00 AM");
+        List b =new ArrayList<String>();
+        b.add("BasketBall Game");
+        b.add("Echandens");
+        b.add("14:00 PM");
+        List c=new ArrayList<List<String>>();
+                c.add(a);
+        c.add(b);
+       MyAdapter  adapter = new MyAdapter(getContext(), c);
 
         // Set the adapter to the ListView
         listView.setAdapter(adapter);
@@ -55,11 +64,137 @@ public class ScheduleFragment extends Fragment {
         rectangle14Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity mainActivity = (MainActivity) getActivity();
-                if (mainActivity != null) {
-                  //  TextView myTextView = mainActivity.findViewById(R.id.football_game);
-                    //myTextView.setText("New text");
-                }
+                List a =new ArrayList<String>();
+                a.add("Tennis Match");
+                a.add("Unil-Sport");
+                a.add("11:00 AM");
+                TextView dateText=view.findViewById(R.id._7_march___2023);
+                dateText.setText("16 March , 2023");
+
+                List c=new ArrayList<List<String>>();
+                c.add(a);
+                MyAdapter  adapter = new MyAdapter(getContext(), c);
+                ListView listView = (ListView) view.findViewById(R.id.list_view);
+                // Set the adapter to the ListView
+                listView.setAdapter(adapter);
+            }
+        });
+        Button rectangle15Button = view.findViewById(R.id.rectangle_15);
+
+        rectangle15Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List a =new ArrayList<String>();
+                a.add("HandBall Game");
+                a.add("Chavannes-Epenex");
+                a.add("02:00 PM");
+                TextView dateText=view.findViewById(R.id._7_march___2023);
+                dateText.setText("17 March , 2023");
+
+                List c=new ArrayList<List<String>>();
+                c.add(a);
+                MyAdapter  adapter = new MyAdapter(getContext(), c);
+
+                ListView listView = (ListView) view.findViewById(R.id.list_view);
+                // Set the adapter to the ListView
+                listView.setAdapter(adapter);
+            }
+        });
+        Button rectangle16Button = view.findViewById(R.id.rectangle_16);
+
+        rectangle16Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List a =new ArrayList<String>();
+                a.add("Football Game");
+                a.add("Unil-Sport");
+                a.add("05:00 PM");
+                TextView dateText=view.findViewById(R.id._7_march___2023);
+                dateText.setText("18 March , 2023");
+                List c=new ArrayList<List<String>>();
+                c.add(a);
+                MyAdapter  adapter = new MyAdapter(getContext(), c);
+                ListView listView = (ListView) view.findViewById(R.id.list_view);
+                // Set the adapter to the ListView
+                listView.setAdapter(adapter);
+            }
+        });
+        Button rectangle11Button = view.findViewById(R.id.rectangle_11);
+
+        rectangle11Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView dateText=view.findViewById(R.id._7_march___2023);
+                dateText.setText("15 March , 2023");
+
+                List c=new ArrayList<List<String>>();
+                MyAdapter  adapter = new MyAdapter(getContext(), c);
+                ListView listView = (ListView) view.findViewById(R.id.list_view);
+                // Set the adapter to the ListView
+                listView.setAdapter(adapter);
+            }
+        });
+        Button rectangle18Button = view.findViewById(R.id.rectangle_18);
+
+        rectangle18Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListView listView = (ListView) view.findViewById(R.id.list_view);
+
+                List a =new ArrayList<String>();
+                a.add("Football Game");
+                a.add("Unil-Sport");
+                a.add("10:00 AM");
+                List b =new ArrayList<String>();
+                b.add("BasketBall Game");
+                b.add("Echandens");
+                b.add("14:00 PM");
+                List c=new ArrayList<List<String>>();
+                c.add(a);
+                c.add(b);
+                MyAdapter  adapter = new MyAdapter(getContext(), c);
+                TextView dateText=view.findViewById(R.id._7_march___2023);
+                dateText.setText("12 March , 2023");
+                // Set the adapter to the ListView
+                listView.setAdapter(adapter);
+            }
+        });
+        Button rectangle3Button = view.findViewById(R.id.rectangle_3);
+
+        rectangle3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListView listView = (ListView) view.findViewById(R.id.list_view);
+
+                List a =new ArrayList<String>();
+                a.add("Football Game");
+                a.add("Unil-Sport");
+                a.add("07:00 PM");
+                TextView dateText=view.findViewById(R.id._7_march___2023);
+                dateText.setText("14 March , 2023");
+                List c=new ArrayList<List<String>>();
+                c.add(a);
+                MyAdapter  adapter = new MyAdapter(getContext(), c);
+
+                // Set the adapter to the ListView
+                listView.setAdapter(adapter);
+            }
+        });
+        Button rectangle17Button = view.findViewById(R.id.rectangle_17);
+
+        rectangle17Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListView listView = (ListView) view.findViewById(R.id.list_view);
+                TextView dateText=view.findViewById(R.id._7_march___2023);
+                dateText.setText("13 March , 2023");
+
+
+                List c=new ArrayList<List<String>>();
+                MyAdapter  adapter = new MyAdapter(getContext(), c);
+
+                // Set the adapter to the ListView
+                listView.setAdapter(adapter);
             }
         });
     }
