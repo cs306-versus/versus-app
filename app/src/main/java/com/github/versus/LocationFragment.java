35c0525109a7ed56880e5a03a25d108115dba19d
unknown <adam.mernissiarifi@epfl.ch>
2023-03-27 03:57:03 +0200

    Filter by Radius

--- app/src/main/java/com/github/versus/LocationFragment.java
+++ app/src/main/java/com/github/versus/LocationFragment.java
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,7 +14,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +29,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.github.versus.user.CustomPlace;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -36,9 +43,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * An activity that displays a map showing the place at the device's current location.
@@ -70,19 +88,19 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
     public static CircleOptions circleOptions;//TODO See if its a good idea
     private EditText editTextRadius;
     private Location lastKnownLocation;
-    private static LatLng epfl ;
-    private static LatLng sat;
-    public static MarkerOptions epflMarker ;
-    public static MarkerOptions satelliteMarker;
+    private static LatLng epfl;
+    private float radius ;
+    public static MarkerOptions epflMarker;
 
-
-    /*These attributes will be used later
     private static final int M_MAX_ENTRIES = 5;
     private String[] likelyPlaceNames;
     private String[] likelyPlaceAddresses;
     private List[] likelyPlaceAttributions;
     private LatLng[] likelyPlaceLatLngs;
-    */
+    private AlertDialog placesDialog ;
+    private boolean firstTime =true;
+    private boolean hasLocations =false;
+
 
 
     @Override
@@ -99,12 +117,9 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
         }
         // Get the EditText view for the radius
 
-        // Construct a PlacesClient
+        // Construct a PlacesClient and retrieve the API Key from local.properties file
         try {
-            String API_KEY = getContext()
-                    .getPackageManager()
-                    .getApplicationInfo(getContext().getPackageName(), PackageManager.GET_META_DATA)
-                    .metaData.getString("com.google.android.geo.API_KEY");
+            String API_KEY = getContext().getPackageManager().getApplicationInfo(getContext().getPackageName(), PackageManager.GET_META_DATA).metaData.getString("com.google.android.geo.API_KEY");
             Places.initialize(getActivity().getApplicationContext(), API_KEY);
 
         } catch (PackageManager.NameNotFoundException e) {
@@ -154,11 +169,11 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
         // Add markers for EPFL and Satellite
         epfl = new LatLng(46.520536, 6.568318);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(epfl, 15));
-        sat = new LatLng(46.520544, 6.567825);
-        epflMarker=new MarkerOptions().position(epfl).title("EPFL");
-        satelliteMarker=new MarkerOptions().position(sat).title("Sat");
+
+        epflMarker = new MarkerOptions().position(epfl).title("EPFL");
+
         map.addMarker(epflMarker);
-        map.addMarker(satelliteMarker);
+
 
         map.moveCamera(CameraUpdateFactory.newLatLng(epfl));
         map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
@@ -225,6 +240,10 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.option_get_place) {
+            showCurrentPlace(radius);
+        }
+
         if (item.getItemId() == R.id.option_radius) {
             chooseRadius();
         }
@@ -244,10 +263,10 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 // Get the radius entered by the user
-                float radius = Float.parseFloat(editText.getText().toString());
+              radius = Float.parseFloat(editText.getText().toString());
 
                 // Update the CircleOptions object with the new radius and redraw the circle
-                if(circleOptions!=null){
+                if (circleOptions != null) {
                     circleOptions.radius(radius);
                     map.clear();
                     map.addCircle(circleOptions);
@@ -258,13 +277,6 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
         dialog.show();
     }
-    /**
-     * Gets the current location of the device for test purposes
-     */
-    public static LatLng getLocalPos(){
-        return localPos;
-    }
-
 
     /**
      * Gets the current location of the device, and positions the map's camera.
@@ -307,7 +319,6 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
             Log.e("Exception: %s", e.getMessage(), e);
         }
     }
-
 
 
     /**
@@ -366,15 +377,8 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
             Log.e("Exception: %s", e.getMessage());
         }
     }
-}
 
-/**************** Methods that are not needed yet************************************************/
-
-
-
-   /*
-
-    private void openPlacesDialog() {
+   /* private void openPlacesDialog() {
         // Ask the user to choose the place where they are now.
         DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
             @Override
@@ -388,86 +392,171 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
                 // Add a marker for the selected place, with an info window
                 // showing information about that place.
-                map.addMarker(new MarkerOptions()
-                        .title(likelyPlaceNames[which])
-                        .position(markerLatLng)
-                        .snippet(markerSnippet));
+                map.addMarker(new MarkerOptions().title(likelyPlaceNames[which]).position(markerLatLng).snippet(markerSnippet));
+
 
                 // Position the map's camera at the location of the marker.
-                map.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLatLng,
-                        DEFAULT_ZOOM));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLatLng, DEFAULT_ZOOM));
             }
         };
 
         // Display the dialog.
-        AlertDialog dialog = new AlertDialog.Builder(getActivity())
-                .setTitle(R.string.pick_place)
-                .setItems(likelyPlaceNames, listener)
-                .show();
+        AlertDialog dialog = new AlertDialog.Builder(getActivity()).setTitle(R.string.pick_place).setItems(likelyPlaceNames, listener).show();
+    }*/
+   private void openPlacesDialog() {
+       AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+       builder.setTitle("Select a place");
+
+       // Add an EditText to get the radius value
+       LinearLayout layout = new LinearLayout(requireActivity());
+       layout.setOrientation(LinearLayout.VERTICAL);
+
+       final EditText radiusInput = new EditText(requireActivity());
+       radiusInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+       radiusInput.setHint("Enter radius (in meters)");
+       layout.addView(radiusInput);
+
+       final ListView listView = new ListView(requireActivity());
+       listView.setAdapter(new ArrayAdapter<>(requireActivity(), android.R.layout.simple_list_item_1, likelyPlaceNames));
+       layout.addView(listView);
+       builder.setView(layout);
+
+
+
+
+
+
+
+
+
+
+
+       // Set the listener for the list view
+       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               // The user has selected a place. Add a marker and move the camera to the selected place.
+               LatLng selectedPlace = likelyPlaceLatLngs[position];
+               map.addMarker(new MarkerOptions()
+                       .title(likelyPlaceNames[position])
+                       .position(selectedPlace)
+                       .snippet(likelyPlaceAddresses[position]));
+               map.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedPlace, DEFAULT_ZOOM));
+               placesDialog.dismiss();
+           }
+       });
+
+       // Set the positive button to filter locations by radius
+       builder.setPositiveButton("Show Places", new DialogInterface.OnClickListener() {
+           @Override
+           public void onClick(DialogInterface dialog, int which) {
+               String radiusStr = radiusInput.getText().toString();
+               if (!TextUtils.isEmpty(radiusStr)) {
+                   double radius = Double.parseDouble(radiusStr);
+
+                   showCurrentPlace(radius);
+               } else {
+                   Toast.makeText(requireActivity(), "Please enter a radius", Toast.LENGTH_SHORT).show();
+               }
+           }
+       });
+
+
+
+       // Set the negative button
+       builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+           @Override
+           public void onClick(DialogInterface dialog, int which) {
+               dialog.dismiss();
+           }
+       });
+
+       placesDialog = builder.create(); // Initialize the placesDialog variable here
+       placesDialog.show();
+   }
+    private void showToast(String message) {
+        LayoutInflater inflater = getLayoutInflater();
+        View toastLayout = inflater.inflate(R.layout.custom_toast, (ViewGroup) requireView().findViewById(R.id.custom_toast_root));
+
+        TextView toastText = toastLayout.findViewById(R.id.custom_toast_text);
+        toastText.setText(message);
+
+        Toast toast = new Toast(requireActivity());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(toastLayout);
+        toast.show();
     }
 
-
-}
-
- private void showCurrentPlace() {
+    private void showCurrentPlace(double radius) { // Add radius parameter to the method
         if (map == null) {
             return;
         }
+        List<CustomPlace> customPlaces = Arrays.asList(new CustomPlace("Football Unil", "Football Unil", new LatLng(46.519385, 6.580856)),
+                new CustomPlace("Football Chavannes", "Football Chavannes", new LatLng(46.52527373363714, 6.57366257779824))
+
+        );
 
         if (locationPermissionGranted) {
-            // Use fields to define the data types to return.
-            List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS,
-                    Place.Field.LAT_LNG);
 
-            // Use the builder to create a FindCurrentPlaceRequest.
-            FindCurrentPlaceRequest request =
-                    FindCurrentPlaceRequest.newInstance(placeFields);
+            FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+            if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Task<Location> lastLocation = fusedLocationClient.getLastLocation();
 
-            // Get the likely places - that is, the businesses and other points of interest that
-            // are the best match for the device's current location.
-            @SuppressWarnings("MissingPermission") final
-            Task<FindCurrentPlaceResponse> placeResult =
-                    placesClient.findCurrentPlace(request);
-            placeResult.addOnCompleteListener (new OnCompleteListener<FindCurrentPlaceResponse>() {
+
+            lastLocation.addOnSuccessListener(new OnSuccessListener<Location>() {
                 @Override
-                public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
-                    if (task.isSuccessful() && task.getResult() != null) {
-                        FindCurrentPlaceResponse likelyPlaces = task.getResult();
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        LatLng userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
 
-                        // Set the count, handling cases where less than 5 entries are returned.
-                        int count;
-                        if (likelyPlaces.getPlaceLikelihoods().size() < M_MAX_ENTRIES) {
-                            count = likelyPlaces.getPlaceLikelihoods().size();
-                        } else {
-                            count = M_MAX_ENTRIES;
+                        List<CustomPlace> filteredPlaces = new ArrayList<>();
+                        for (CustomPlace customPlace : customPlaces) {
+                            double distance = haversineDistance(userLatLng, customPlace.latLng);
+
+                            if (distance <= radius) {
+                                filteredPlaces.add(customPlace);
+                                hasLocations=true;
+                            }
+                            else{
+                                if(!hasLocations && radius != 0){
+                                    showToast("No locations found within the selected radius");
+                                    firstTime=false;
+                                }
+
+                            }
                         }
 
-                        int i = 0;
+                        hasLocations=false;
+
+                        int count = filteredPlaces.size();
+
                         likelyPlaceNames = new String[count];
                         likelyPlaceAddresses = new String[count];
                         likelyPlaceAttributions = new List[count];
                         likelyPlaceLatLngs = new LatLng[count];
 
-                        for (PlaceLikelihood placeLikelihood : likelyPlaces.getPlaceLikelihoods()) {
-                            // Build a list of likely places to show the user.
-                            likelyPlaceNames[i] = placeLikelihood.getPlace().getName();
-                            likelyPlaceAddresses[i] = placeLikelihood.getPlace().getAddress();
-                            likelyPlaceAttributions[i] = placeLikelihood.getPlace()
-                                    .getAttributions();
-                            likelyPlaceLatLngs[i] = placeLikelihood.getPlace().getLatLng();
-
-                            i++;
-                            if (i > (count - 1)) {
-                                break;
-                            }
+                        for (int i = 0; i < count; i++) {
+                            CustomPlace customPlace = filteredPlaces.get(i);
+                            likelyPlaceNames[i] = customPlace.name;
+                            likelyPlaceAddresses[i] = customPlace.address;
+                            likelyPlaceLatLngs[i] = customPlace.latLng;
                         }
 
-                        // Show a dialog offering the user the list of likely places, and add a
+                        // Show a dialog offering the user the list of custom places, and add a
                         // marker at the selected place.
-                        MapsActivityCurrentPlace.this.openPlacesDialog();
-                    }
-                    else {
-                        Log.e(TAG, "Exception: %s", task.getException());
+
+                                LocationFragment.this.openPlacesDialog();
+
+
                     }
                 }
             });
@@ -485,4 +574,18 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
             getLocationPermission();
         }
     }
-*/
+
+
+    public static double haversineDistance(LatLng latLng1, LatLng latLng2) {
+        double earthRadius = 6371; // Radius of the earth in km
+        double dLat = Math.toRadians(latLng2.latitude - latLng1.latitude);
+        double dLng = Math.toRadians(latLng2.longitude - latLng1.longitude);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(latLng1.latitude)) * Math.cos(Math.toRadians(latLng2.latitude))
+                * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return earthRadius * c * 1000; // Distance in km
+    }
+
+
+}
