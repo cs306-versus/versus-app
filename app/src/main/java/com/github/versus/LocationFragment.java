454733c49e3bcb140e73697a550328101fc169e8
unknown <adam.mernissiarifi@epfl.ch>
2023-03-29 05:27:20 +0200

    Tests

--- app/src/main/java/com/github/versus/LocationFragment.java
+++ app/src/main/java/com/github/versus/LocationFragment.java
@@ -245,7 +245,7 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
         }
         if (item.getItemId() == R.id.option_radius) {
-            chooseRadius();
+            openPlacesDialog();
 
         }
 
@@ -608,24 +608,20 @@ private void openPlacesDialog(){
 
     // Get a reference to the EditText view in the layout
     EditText radiusInput = view.findViewById(R.id.edit_text_radius2);
-    radiusInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
-    radiusInput.setHint("Enter radius (in meters)");
+    //radiusInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+    //radiusInput.setHint("Enter radius (in meters)");
 
     // Create a dialog to display the EditText view
     AlertDialog dialog = new AlertDialog.Builder(getActivity()).setTitle("Enter radius").setView(view).setPositiveButton("OK", new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialog, int which) {
              // Get the radius entered by the user
-            String radiusStr = radiusInput.getText().toString();
-            if (!TextUtils.isEmpty(radiusStr)) {
+
                 radius = Float.parseFloat(radiusInput.getText().toString());
 
                 drawCircle(radius);
                 showCurrentPlace(radius);
-            }
-            else {
-                showToast("Please enter a valid radius");
-            }
+
 
         }
     }).setNegativeButton("Cancel", null).create();
