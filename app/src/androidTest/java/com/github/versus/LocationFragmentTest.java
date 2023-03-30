8cd1b7617ed1354564e54c9fcd75133367c4d6ea
Alexander Mueller <alexander.muller@epfl.ch>
2023-03-31 01:27:26 +0200

    re-enable tests after fixing settings-json

--- app/src/androidTest/java/com/github/versus/LocationFragmentTest.java
+++ app/src/androidTest/java/com/github/versus/LocationFragmentTest.java
@@ -81,11 +81,11 @@ public class LocationFragmentTest {
     @Test
     public void testUiElements() throws InterruptedException {
         // Find the overflow menu button and find the "Choose a radius" menu item and perform a type text action
-//        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
 //        // Find the menu item by its ID and perform a click
-//        onView(withText("Choose a radius")).perform(click());
-//        onView(withId(R.id.edit_text_radius)).perform(typeText("1000"));
-//        onView(withText("OK")).perform(click());
+        onView(withText("Choose a radius")).perform(click());
+        onView(withId(R.id.edit_text_radius)).perform(typeText("1000"));
+        onView(withText("OK")).perform(click());
         //Testing circle properties
 
 
