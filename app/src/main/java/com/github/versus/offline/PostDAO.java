7ab70f967d9565e9d793badf4ffec64a05f39fe0
ziazi <mehdi.ziazi@epfl.ch>
2023-03-30 22:39:08 +0200

    added more queries (select by sport and randomly ) with their tests

--- app/src/main/java/com/github/versus/offline/PostDAO.java
+++ app/src/main/java/com/github/versus/offline/PostDAO.java
@@ -19,23 +19,19 @@ public interface  PostDAO  {
     @Query("SELECT * FROM CachedPost WHERE id IN (:ids)")
     List<CachedPost> loadAllByIds(String ids[]);
 
-    /*
+
     @Query("SELECT * FROM CachedPost ORDER BY RANDOM() LIMIT 1")
     CachedPost randomSelect();
-    */
+
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(CachedPost... rows);
 
+
+    @Query("SELECT * FROM CachedPost WHERE sport LIKE :sport ")
+    CachedPost loadBySport(String sport);
+
     @Query("DELETE FROM CachedPost WHERE id LIKE :id")
     void deleteById(String id);
-
-
-    /*
-    @Delete
-    void delete(CachedPost row);
-
-     */
-
 
 }
