30bff83629dd0edf123b4bf78151d5f4c64a6f02
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-03-31 04:16:43 +0200

    Switch testing strategy for UserInfoFragment

--- app/src/androidTest/java/com/github/versus/user/UserInfoFragmentTest.java
+++ app/src/androidTest/java/com/github/versus/user/UserInfoFragmentTest.java
@@ -10,6 +10,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.StringContains.containsString;
 
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.DrawerMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -17,6 +18,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.github.versus.MainActivity;
 import com.github.versus.R;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Before;
@@ -24,18 +26,20 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-//@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4.class)
 public class UserInfoFragmentTest {
 
-    /*@Rule
+    @Rule
     public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
 
     @Before
     public void setUpFragment(){
-        FirebaseAuth.getInstance().signInWithEmailAndPassword("test@versus.ch", "123456789");
-        onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isClosed(GravityCompat.START))).perform(DrawerActions.open());
-        onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen(GravityCompat.START)));
-        onView(withId(R.id.nav_user_profil)).perform(click());
+        Task<?> task = FirebaseAuth.getInstance().signInWithEmailAndPassword("test@versus.ch", "123456789");
+        while(!task.isComplete() && !task.isCanceled());
+        setUp();
+        //onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isClosed(GravityCompat.START))).perform(DrawerActions.open());
+        //onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen(GravityCompat.START)));
+        //onView(withId(R.id.nav_user_profil)).perform(click());
     }
 
     @Test
@@ -43,6 +47,13 @@ public class UserInfoFragmentTest {
         onView(withId(R.id.info_uid)).check(matches(withText(containsString("EPPOpGluoEQe6OsYZJ96mvZ1Ytu2"))));
     }
 
+    private void setUp() {
+        scenario.getScenario().onActivity(activity -> {
+            FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+            UserInfoFragment voiceFragment = new UserInfoFragment();
+            transaction.add(voiceFragment, "voiceFragment");
+            transaction.commit();
+        });
+    }
 
-     */
 }
