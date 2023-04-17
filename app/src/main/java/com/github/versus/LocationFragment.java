dd6d0ff51ba1950c615320ef5f83e5538900c9c2
unknown <adam.mernissiarifi@epfl.ch>
2023-04-17 21:13:02 +0200

    Test

--- app/src/main/java/com/github/versus/LocationFragment.java
+++ app/src/main/java/com/github/versus/LocationFragment.java
@@ -411,9 +411,7 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
      * @param radius The radius (in meters) around the user's current location to search for custom places.
      */
     public void showCurrentPlace(double radius) {
-        if (map == null) {
-            return;
-        }
+
         List<CustomPlace> customPlaces = Arrays.asList(new CustomPlace("UNIL Football", "UNIL Football", new LatLng(46.519385, 6.580856)),
                 new CustomPlace("Chavannes Football", "Chavannes Football", new LatLng(46.52527373363714, 6.57366257779824)),
                 new CustomPlace("Bassenges Football","Bassenges Football",new LatLng(46.52309381914529, 6.5608807098372175)),
@@ -422,7 +420,7 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
         );
 
-        if (locationPermissionGranted) {
+        if (true) {
 
             FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
             @SuppressWarnings("MissingPermission") Task<Location> lastLocation = fusedLocationClient.getLastLocation();
