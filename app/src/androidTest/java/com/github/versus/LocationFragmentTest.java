af7526fed9882b19a6704fd94c891e3749d92f3a
unknown <adam.mernissiarifi@epfl.ch>
2023-04-18 01:38:03 +0200

    Test

--- app/src/androidTest/java/com/github/versus/LocationFragmentTest.java
+++ app/src/androidTest/java/com/github/versus/LocationFragmentTest.java
@@ -162,10 +162,7 @@ public class LocationFragmentTest {
         onView(withText("Show Places")).inRoot(isDialog())
                 .perform(click());
 
-        onView(withText("Cancel")).inRoot(isDialog())
-
-
-                .perform(click());
+        onView(withText("Cancel")).inRoot(isDialog()).perform(click());
 
 
 
