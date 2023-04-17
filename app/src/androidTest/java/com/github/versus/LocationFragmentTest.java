23c5fc348e12ccbe0cc6d8a161d41d8e073f8a17
unknown <adam.mernissiarifi@epfl.ch>
2023-04-17 20:24:24 +0200

    Test

--- app/src/androidTest/java/com/github/versus/LocationFragmentTest.java
+++ app/src/androidTest/java/com/github/versus/LocationFragmentTest.java
@@ -158,13 +158,7 @@ public class LocationFragmentTest {
 
         onView(withId(R.id.edit_text_radius2)).perform(typeText("800"));
         closeSoftKeyboard();
-
-
-
         onView(withText("Show Places")).perform(click());
-
-
-
         onView(withText("Cancel")).perform(click());
 
 
