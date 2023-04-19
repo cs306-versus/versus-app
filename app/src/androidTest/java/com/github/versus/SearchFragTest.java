0c4956a183af7ddf438144fbbaa881ff1e503236
Alexander Mueller <alexander.muller@epfl.ch>
2023-04-19 18:44:07 +0200

    change tests to reflect string updates

--- app/src/androidTest/java/com/github/versus/SearchFragTest.java
+++ app/src/androidTest/java/com/github/versus/SearchFragTest.java
@@ -8,6 +8,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static com.google.common.io.Resources.getResource;
 import static org.hamcrest.Matchers.containsString;
 
 import androidx.core.view.GravityCompat;
@@ -53,9 +54,9 @@ public class SearchFragTest {
         onView(withId(R.id.editPostTitle)).check(matches(isDisplayed()));
         onView(withId(R.id.editPostTitle)).check(matches(withText(containsString("Title"))));
         onView(withId(android.R.id.button1)).perform(click());
-        onView(withText(containsString("choose a sport"))).check(matches(isDisplayed()));
-        onView(withText(containsString("next"))).perform(click());
-        onView(withText(containsString("cancel"))).perform(click());
+        onView(withText(containsString( "Choose a Sport"))).check(matches(isDisplayed()));
+        onView(withText(containsString("Next"))).perform(click());
+        onView(withText(containsString("Cancel"))).perform(click());
 
     }
 }
