3e9700392c6a6b2b6894d0fff6e655e77dbd3f22
Alexander Mueller <alexander.muller@epfl.ch>
2023-04-19 19:22:53 +0200

    add filter for titles and sport

--- app/src/main/java/com/github/versus/SearchFragment.java
+++ app/src/main/java/com/github/versus/SearchFragment.java
@@ -2,10 +2,13 @@ package com.github.versus;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,6 +33,7 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class SearchFragment extends Fragment implements
         CreatePostTitleDialogFragment.TitleListener,
@@ -40,12 +44,16 @@ public class SearchFragment extends Fragment implements
 
     protected RecyclerView recyclerView;
     protected Post newPost;
+
+    protected EditText searchBar;
     protected CreatePostTitleDialogFragment cpdf;
     protected ChoosePostSportDialogFragment cpsdf;
     protected MaxPlayerDialogFragment mpdf;
     protected PostDatePickerDialog pdpd;
 
     protected List<Post> posts = new ArrayList<>();
+    protected List<Post> displayPosts = new ArrayList<>();
+    protected String filter = "";
 
     protected AnnouncementAdapter aa;
     protected FsPostManager pm;
@@ -61,8 +69,21 @@ public class SearchFragment extends Fragment implements
         mpdf = new MaxPlayerDialogFragment();
         pdpd = new PostDatePickerDialog();
         pm = new FsPostManager(FirebaseFirestore.getInstance());
+        searchBar = rootView.findViewById(R.id.search_posts);
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                filterPosts();
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
 
+            }
+        });
         Button addPost = (Button) rootView.findViewById(R.id.add_posts);
         addPost.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,7 +92,7 @@ public class SearchFragment extends Fragment implements
             }
         });
 
-        aa = new AnnouncementAdapter(posts);
+        aa = new AnnouncementAdapter(displayPosts);
         loadPosts();
         LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
@@ -116,10 +137,32 @@ public class SearchFragment extends Fragment implements
         postsFuture.thenApply(newPosts -> {
             posts.clear();
             posts.addAll(newPosts);
-            System.out.println(posts.size());
-            aa.notifyDataSetChanged();
+            filterPosts();
             return posts;
         });
+    }
+
+    protected void filterPosts(){
+        filter = searchBar.getText().toString();
+        displayPosts.clear();
+        if(filter.length() == 0){
+            displayPosts.addAll(posts);
+        }
+        else {
+            displayPosts.addAll(
+              posts.stream().filter(post -> {
+                  return post.getSport().name().toLowerCase().contains(filter.toLowerCase())
+                          || post.getTitle().toLowerCase().contains(filter.toLowerCase());
+              }).collect(Collectors.toList())
+            );
+        }
+
+        aa.notifyDataSetChanged();
+    }
+
+    protected void clearFilter(){
+        searchBar.setText("");
+        filterPosts();
     }
 
     @Override
