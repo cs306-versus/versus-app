70a4283882f5d351ee2a9c6a2d44dc240ed62c69
ziazi <mehdi.ziazi@epfl.ch>
2023-04-20 18:11:13 +0200

    made sport converter fail withoout exception and added tests

--- app/src/test/java/com/github/versus/SportConverterTest.java
+++ app/src/test/java/com/github/versus/SportConverterTest.java
@@ -2,22 +2,36 @@ package com.github.versus;
 
 import static org.junit.Assert.assertTrue;
 
+import com.github.versus.offline.SportConverter;
 import com.github.versus.offline.UserConverter;
 import com.github.versus.sports.Sport;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class SportConverterTest {
 
     @Test
+    public void convertSportWorks(){
+        assertTrue(SportConverter.convertSport(Sport.BOXING).equals(Sport.BOXING.name()));
+    }
+    @Test
+    public void convertBackSportFailsWithNoException(){
+        assertTrue(SportConverter.convertSportBack("no sport")==null);
+    }
+    @Test
+    public void convertBackSportWorks(){
+        assertTrue(SportConverter.convertSportBack(Sport.BOXING.name()).equals(Sport.BOXING));
+    }
+
+    @Test
     public void convertListOfSportsWorks() {
         Sport sports[]= {Sport.FOOTBALL,Sport.CLIMBING,Sport.ROWING,Sport.SOCCER};
         String sep = "***";
-        String conversion = UserConverter.convertListOfSports(Arrays.asList(sports),sep);
+        String conversion = SportConverter.convertListOfSports(Arrays.asList(sports),sep);
         String shouldBe= Sport.FOOTBALL.name()+sep+Sport.CLIMBING.name()+sep+Sport.ROWING.name()+sep+Sport.SOCCER.name();
         assertTrue(conversion.equals(shouldBe));
     }
@@ -26,7 +40,7 @@ public class SportConverterTest {
     public void convertListOnEmptyStrings() {
         Sport sports[]= {};
         String sep = "***";
-        String conversion = UserConverter.convertListOfSports(Arrays.asList(sports),sep);
+        String conversion = SportConverter.convertListOfSports(Arrays.asList(sports),sep);
         String shouldBe= "";
         assertTrue(conversion.equals(shouldBe));
     }
@@ -34,29 +48,27 @@ public class SportConverterTest {
     public void convertBackToSportsWorks(){
         String sep = "&";
         String conversion ="FOOTBALL&CLIMBING&ROWING&SOCCER";
-        List<Sport> sportList= UserConverter.convertBackToSports(conversion,sep);
+        List<Sport> sportList= SportConverter.convertBackToSports(conversion,sep);
         Sport shouldBe[]= {Sport.FOOTBALL,Sport.CLIMBING,Sport.ROWING,Sport.SOCCER};
         System.out.println(sportList);
         assertTrue(sportList.equals(Arrays.asList(shouldBe)));
 
-    }
-    @Test
-    public void convertBackToSportsWorksWithEmptyString(){
-        String sep = "&";
-        String conversion ="";
-        List<Sport> sportList= new ArrayList<>();
-        System.out.println(sportList);
-        Sport shouldBe[]= {};
-        assertTrue(sportList.equals(Arrays.asList(shouldBe)));
     }
 
     @Test
     public void convertBackToSportsIsTheInverseOfConvertListOfSports(){
         Sport sports[]= {Sport.FOOTBALL,Sport.CLIMBING,Sport.ROWING,Sport.SOCCER};
         String sep = "&";
-        String conversion = UserConverter.convertListOfSports(Arrays.asList(sports),sep);
-        List<Sport> sportList= UserConverter.convertBackToSports(conversion,sep);
+        String conversion = SportConverter.convertListOfSports(Arrays.asList(sports),sep);
+        List<Sport> sportList= SportConverter.convertBackToSports(conversion,sep);
         assertTrue(sportList.equals(Arrays.asList(sports)));
 
     }
+    /*
+    @Test
+    public void convertBackFailsWithNoException(){
+        assertTrue();
+    }
+    */
+
 }
