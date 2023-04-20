ce45e4cf001114eee121e6847d15f9a91df9187b
ziazi <mehdi.ziazi@epfl.ch>
2023-04-20 13:53:47 +0200

    more comments

--- app/src/main/java/com/github/versus/offline/NetworkManager.java
+++ app/src/main/java/com/github/versus/offline/NetworkManager.java
@@ -4,9 +4,18 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
 
+/**
+ * A class that manages the internet connectivity of the application
+ */
 public final class NetworkManager {
-
     private NetworkManager(){}
+
+    /**
+     * Returns if the app has access to network connection
+     * @param context: the context of the application
+     * @return
+     * true iff the application has access to network connection.
+     */
     public static boolean isNetworkAvailable(Context context) {
         boolean connected = false;
         ConnectivityManager connectivityManager = (ConnectivityManager) context.
