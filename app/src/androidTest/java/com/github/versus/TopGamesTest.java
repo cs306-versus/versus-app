7cc71b20df6375a0115f7385092e3ed9f35c8504
unknown <aymane.lamyaghri@epfl.ch>
2023-05-02 11:54:55 +0200

    Working one

--- app/src/androidTest/java/com/github/versus/TopGamesTest.java
+++ app/src/androidTest/java/com/github/versus/TopGamesTest.java
@@ -63,6 +63,7 @@ public class TopGamesTest {
     @Before
     public void setUp() {
         Intents.init();
+        
     }
 
     @After
--- app/src/main/java/com/github/versus/TrendingGamesFragment.java
+++ app/src/main/java/com/github/versus/TrendingGamesFragment.java
@@ -15,50 +15,67 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 public class TrendingGamesFragment extends Fragment {
-    private int  currentPositionInRating;
-    private TrendingGamesManager  gamesManager ;
 
-    public TrendingGamesFragment(){
-        currentPositionInRating=1;
-        gamesManager=new TrendingGamesManager();
+
+    // Represents the current position in the rating of trending games
+    private int currentPositionInRating;
+
+    // Manages the trending games data, such as sport names and images
+    private TrendingGamesManager gamesManager;
+
+
+    /**
+     * The constructor initializes the currentPositionInRating to 1 and creates a new instance of TrendingGamesManager.
+     */
+    public TrendingGamesFragment() {
+        currentPositionInRating = 1;
+        gamesManager = new TrendingGamesManager();
 
     }
+
+    /**
+     * The onCreateView method inflates the view from the fragment_trendingsports.xml layout file.
+     * It sets the visibility of the gold medal to visible and the visibility of the silver and bronze medals to invisible.
+     * It also sets click listeners for the left and right arrows and the image view.
+     *
+     * @param inflater           - The LayoutInflater object that can be used to inflate any views in the fragment
+     * @param container          - The parent view that the fragment's UI should be attached to
+     * @param savedInstanceState - This fragment is being re-constructed from a previous saved state as given here.
+     * @return - the inflated view
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_trendingsports,container,false);
+        View view = inflater.inflate(R.layout.fragment_trendingsports, container, false);
         ImageView imageView = view.findViewById(R.id.rectangle_22);
-        ImageView gold_medal=view.findViewById(R.id.medalgold_champion_award_winner_olympic_icon_1);
-        ImageView silver_medal=view.findViewById(R.id.medalsilver_champion_award_winner_olympic_icon_1);
-        ImageView bronze_medal=view.findViewById(R.id.medalbronze_champion_award_winner_olympic_icon_1);
+        ImageView gold_medal = view.findViewById(R.id.medalgold_champion_award_winner_olympic_icon_1);
+        ImageView silver_medal = view.findViewById(R.id.medalsilver_champion_award_winner_olympic_icon_1);
+        ImageView bronze_medal = view.findViewById(R.id.medalbronze_champion_award_winner_olympic_icon_1);
         gold_medal.setVisibility(View.VISIBLE);
         silver_medal.setVisibility(View.INVISIBLE);
         bronze_medal.setVisibility(View.INVISIBLE);
 
 
-
-        ImageView left_arrow=view.findViewById(R.id.arrow_left);
-        ImageView right_arrow=view.findViewById(R.id.arrow_right);
+        ImageView left_arrow = view.findViewById(R.id.arrow_left);
+        ImageView right_arrow = view.findViewById(R.id.arrow_right);
         left_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(currentPositionInRating==1) return ;
-               currentPositionInRating=currentPositionInRating-1;
-               if(currentPositionInRating==1) {
-                   gold_medal.setVisibility(View.VISIBLE);
-                   silver_medal.setVisibility(View.INVISIBLE);
+                if (currentPositionInRating == 1) return;
+                currentPositionInRating = currentPositionInRating - 1;
+                if (currentPositionInRating == 1) {
+                    gold_medal.setVisibility(View.VISIBLE);
+                    silver_medal.setVisibility(View.INVISIBLE);
 
-               }
-                else if(currentPositionInRating==2) {
+                } else if (currentPositionInRating == 2) {
                     silver_medal.setVisibility(View.VISIBLE);
                     bronze_medal.setVisibility(View.INVISIBLE);
 
-                }
-                else if(currentPositionInRating==3){
+                } else if (currentPositionInRating == 3) {
                     bronze_medal.setVisibility(View.VISIBLE);
-               }
+                }
                 imageView.setImageResource(gamesManager.getSportImageAtPos(currentPositionInRating));
-                TextView sport_text =(TextView)view.findViewById(R.id.sport_text);
+                TextView sport_text = (TextView) view.findViewById(R.id.sport_text);
                 sport_text.setText(gamesManager.getSportNameAtPosition(currentPositionInRating));
 
             }
@@ -68,25 +85,23 @@ public class TrendingGamesFragment extends Fragment {
         right_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(currentPositionInRating==5) return ;
-                currentPositionInRating=currentPositionInRating+1;
-                if(currentPositionInRating==2) {
+                if (currentPositionInRating == 5) return;
+                currentPositionInRating = currentPositionInRating + 1;
+                if (currentPositionInRating == 2) {
                     gold_medal.setVisibility(View.INVISIBLE);
                     silver_medal.setVisibility(View.VISIBLE);
 
-                }
-                else if(currentPositionInRating==3) {
+                } else if (currentPositionInRating == 3) {
                     silver_medal.setVisibility(View.INVISIBLE);
                     bronze_medal.setVisibility(View.VISIBLE);
 
-                }
-                else if(currentPositionInRating==4){
+                } else if (currentPositionInRating == 4) {
                     bronze_medal.setVisibility(View.INVISIBLE);
                 }
                 imageView.setImageResource(gamesManager.getSportImageAtPos(currentPositionInRating));
 
                 imageView.invalidate();
-                TextView sport_text =(TextView)view.findViewById(R.id.sport_text);
+                TextView sport_text = (TextView) view.findViewById(R.id.sport_text);
                 sport_text.setText(gamesManager.getSportNameAtPosition(currentPositionInRating));
 
             }
