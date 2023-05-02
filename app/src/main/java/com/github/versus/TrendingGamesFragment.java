df7d5e625d5d876b58b6db709f4b540609e2e509
unknown <aymane.lamyaghri@epfl.ch>
2023-05-02 16:40:57 +0200

    Test

--- app/src/main/java/com/github/versus/TrendingGamesFragment.java
+++ app/src/main/java/com/github/versus/TrendingGamesFragment.java
@@ -28,7 +28,9 @@ public class TrendingGamesFragment extends Fragment {
      * The constructor initializes the currentPositionInRating to 1 and creates a new instance of TrendingGamesManager.
      */
     public TrendingGamesFragment() {
+        //Initialise the Position to the first position, this will be shown by default.
         currentPositionInRating = 1;
+        //Initialize the current TrendingGamesManager
         gamesManager = new TrendingGamesManager();
 
     }
@@ -46,18 +48,37 @@ public class TrendingGamesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_trendingsports, container, false);
+
+        //ImageView that contains the picture of the sport at the current position
         ImageView imageView = view.findViewById(R.id.rectangle_22);
+
+        //Gold medal for the Top1 sport, only visible when the Top1 sport is visible
         ImageView gold_medal = view.findViewById(R.id.medalgold_champion_award_winner_olympic_icon_1);
+
+        //Silver medal for the Top2 sport, only visible when the Top1 sport is visible
         ImageView silver_medal = view.findViewById(R.id.medalsilver_champion_award_winner_olympic_icon_1);
+
+        //Bronze medal for the Top3 sport, only visible when the Top1 sport is visible
         ImageView bronze_medal = view.findViewById(R.id.medalbronze_champion_award_winner_olympic_icon_1);
+
+        //SetVisiblity of the golden medal by default to visible
         gold_medal.setVisibility(View.VISIBLE);
+
+        //SetVisiblity of the silver medal by default to visible
         silver_medal.setVisibility(View.INVISIBLE);
+
+        //SetVisiblity of the bronze medal by default to visible
         bronze_medal.setVisibility(View.INVISIBLE);
 
-
+        //ImageView of the left arrow to switch to next trending sport
         ImageView left_arrow = view.findViewById(R.id.arrow_left);
+
+        //ImageView of the left arrow to switch to next trending sport
         ImageView right_arrow = view.findViewById(R.id.arrow_right);
+
+        //Add the OnClickLister to the leftarrow to switch to next trending sport
         left_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -141,9 +162,14 @@ public class TrendingGamesFragment extends Fragment {
     }
 
 
+    /**
+     * Called after the view hierarchy of the fragment is inflated and ready for use.
+     *
+     * @param view The inflated view of the fragment.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state.
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
     }
 }
