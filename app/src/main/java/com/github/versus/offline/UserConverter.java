db1b74d84b090567ade1d01802092a6f75333772
ziazi <mehdi.ziazi@epfl.ch>
2023-05-04 15:53:44 +0200

    finally a user converter, tests remaining

--- app/src/main/java/com/github/versus/offline/UserConverter.java
+++ app/src/main/java/com/github/versus/offline/UserConverter.java
@@ -2,55 +2,46 @@ package com.github.versus.offline;
 
 import com.github.versus.sports.Sport;
 import com.github.versus.user.User;
+import com.github.versus.user.VersusUser;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class UserConverter {
-    /*
-    public static String convertUser(User user){
-        user.getUID();
-        user.getUserName();
-        user.getFirstName();
-        user.getLastName();
-        user.getMail();
-        user.getPhone();
-        user.getCity();
-        user.getZipCode();
-        user.getRating();
-        user.getPreferredSports();
+    public static String convertUser(User user) {
+        String sportsString= SportConverter.convertListOfSports(user.getPreferredSports());
+        return String.format(Locale.ENGLISH,"%s|%s|%s|%s|%s|%s|%d|%s|%d|%s",
+                user.getUID(), user.getFirstName(), user.getLastName(),
+                user.getUserName(), user.getMail(), user.getPhone(),
+                user.getRating(), user.getCity(), user.getZipCode(),
+                SportConverter.convertListOfSports(user.getPreferredSports()));
 
-        return null ;
-    }*/
-
-    /**
-     * Coverts a list of sports to a string
-     * @param sportList
-     * @param sep
-     * @return
-     * The string representation of the list of sports
-     */
-    public static String convertListOfSports(List<Sport> sportList,String sep){
-        StringBuilder builder = new StringBuilder();
-        for(int i =0 ; i<sportList.size();++i){
-            builder.append(sportList.get(i));
-            if(i<sportList.size()-1){
-                builder.append(sep);
-            }
-        }
-        return builder.toString();
+    }
+    public static User convertBackUser(String userString) {
+        String[] tokens = userString.split("\\|");
+        String uid = tokens[0];
+        String firstName = tokens[1];
+        String lastName = tokens[2];
+        String userName = tokens[3];
+        String mail = tokens[4];
+        String phone = tokens[5];
+        int rating = Integer.parseInt(tokens[6]);
+        String city = tokens[7];
+        int zipCode = Integer.parseInt(tokens[8]);
+        List<Sport> preferredSports = SportConverter.convertBackToSports(tokens[9]);
+        VersusUser.Builder builder = new VersusUser.Builder(uid);
+        builder.setFirstName(firstName);
+        builder.setLastName(lastName);
+        builder.setUserName(userName);
+        builder.setMail(mail);
+        builder.setPhone(phone);
+        builder.setRating(rating);
+        builder.setCity(city);
+        builder.setZipCode(zipCode);
+        builder.setPreferredSports(preferredSports);
+        return new VersusUser(builder);
     }
 
-    /**
-     * Converts a String to a list of sports
-     * @param conversion
-     * @param sep
-     * @return
-     * The string representation of the sport.
-     */
-    public static List<Sport> convertBackToSports(String conversion,String sep){
-        String split[]= conversion.split(sep);
-        return Arrays.asList(split).stream().map(s -> Sport.valueOf(s)).collect(Collectors.toList());
-    }
 }
