e3143e0a43253c1c45bbb58d95f2ae8ee6a3fab3
@DerouichAbdessalam <abdessalam.derouich@epfl.ch>
2023-05-05 04:16:26 +0200

    good test

--- app/src/androidTest/java/com/github/versus/ChatTest.java
+++ app/src/androidTest/java/com/github/versus/ChatTest.java
@@ -1,4 +1,105 @@
+
+
 package com.github.versus;
 
-public class ChatTest {
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static java.util.Objects.requireNonNull;
+
+import android.os.SystemClock;
+
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
+
+
+import android.graphics.Color;
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.DrawerMatchers;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import com.github.versus.auth.AuthActivity;
+
+
+    @RunWith(AndroidJUnit4.class)
+    public class ChatTest{
+
+        //General rule that sets up the Activity Scenario Rule
+        @Rule
+        public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
+
+        //Initiating the intents
+        @Before
+        public void setUp() {
+            Intents.init();
+        }
+
+        //Releasing the intents
+        @After
+        public void tearDown() {
+            Intents.release();
+        }
+
+
+        /**
+         * This method tests the first position of the trending sports by clicking on the appropriate picture and
+         * showing the filtered posts of the sport
+         */
+        @Test
+        public void testOnFirstPosition() {
+            //Opening the drawer menu
+            onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isClosed(GravityCompat.START))).perform(DrawerActions.open());
+            onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen(GravityCompat.START)));
+            //Performing the click on the trending sports button
+            onView(withId(R.id.nav_chats)).perform(click());
+            //Performing multiple clicks  on the left and right arrow in order to cover all the branches  in the code
+
+// Find the RecyclerView by its id
+            ViewInteraction inter = onView(withId(R.id.recyclerView));
+
+// Click on the first item in the RecyclerView
+            inter.perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        }
 }
