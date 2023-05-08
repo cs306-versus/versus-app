5c995ed1b025bbfff9a11349212b088baaa38780
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-05-08 03:14:03 +0200

    Fix EntryActivityTest.java

--- app/src/androidTest/java/com/github/versus/EntryActivityTest.java
+++ app/src/androidTest/java/com/github/versus/EntryActivityTest.java
@@ -2,7 +2,9 @@ package com.github.versus;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
 import com.github.versus.auth.AuthActivity;
 import com.github.versus.utils.FirebaseEmulator;
@@ -11,17 +13,24 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static com.github.versus.utils.auth.EmulatorUserProvider.validMail;
 import static com.github.versus.utils.auth.EmulatorUserProvider.validPassword;
 import static org.junit.Assert.assertTrue;
 
+import android.content.Intent;
+
 @RunWith(AndroidJUnit4.class)
 public class EntryActivityTest {
+
+    @Rule
+    public ActivityTestRule<EntryActivity> rule = new ActivityTestRule<>(EntryActivity.class);
 
     private FirebaseAuth auth;
 
@@ -37,21 +46,24 @@ public class EntryActivityTest {
         Task<AuthResult> task = auth.signInWithEmailAndPassword(validMail(), validPassword());
         while(!(task.isComplete() || task.isCanceled())); // Spin and wait for login
         assertTrue(task.isSuccessful()); // Confirm login
-        try(ActivityScenario<EntryActivity> scenario = ActivityScenario.launch(EntryActivity.class)){
+        try{
             Intents.init();
-            intending(hasComponent(MainActivity.class.getName()));
+            rule.launchActivity(new Intent());
+            intended(hasComponent(MainActivity.class.getName()));
         } finally {
             Intents.release();
         }
+
     }
 
     @Test
     public void switchToAuthActivity(){
         // Sign out
         auth.signOut();
-        try(ActivityScenario<EntryActivity> scenario = ActivityScenario.launch(EntryActivity.class)){
+        try{
             Intents.init();
-            intending(hasComponent(AuthActivity.class.getName()));
+            rule.launchActivity(new Intent());
+            intended(hasComponent(AuthActivity.class.getName()));
         } finally {
             Intents.release();
         }
