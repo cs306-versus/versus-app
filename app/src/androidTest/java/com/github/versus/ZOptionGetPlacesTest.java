594ab9824feedefa6d538f164a3afbf2811e2220
unknown <aymane.lamyaghri@epfl.ch>
2023-05-14 22:31:17 +0200

    Testing

--- app/src/androidTest/java/com/github/versus/ZOptionGetPlacesTest.java
+++ app/src/androidTest/java/com/github/versus/ZOptionGetPlacesTest.java
@@ -34,97 +34,10 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ZOptionGetPlacesTest {
 
-    // Declare activity rule and permission rule
-    @Rule
-    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
-    
-    @Rule
-    public GrantPermissionRule grantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
 
-    /**
-     * Sets up the testing environment before each test.
-     * Initializes the Intents framework and opens the drawer layout.
-     */
-    @Before
-    public void setUp() {
-        // Open the drawer_layout
-        onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isClosed(GravityCompat.START))).perform(DrawerActions.open());
-        onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen(GravityCompat.START)));
-        onView(withId(R.id.nav_location)).perform(click());
-    }
 
     @Test
     public void testDrawingPathWithPlaceSelected() {
-        String placeName = "GooglePlex Football";
-        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
-        // Find the menu item by its ID and perform a click
-        onView(withText("Get Place")).perform(click());
-        onView(withId(R.id.edit_text_radius2)).perform(typeText("1500"), closeSoftKeyboard());
-        onView(withText("Show Places")).inRoot(isDialog()).perform(click());
-        onView(withText(placeName)).perform(click());
-        onView(withText("Draw Path")).perform(click());
-    }
-    
-    @Test
-    public void testDrawingPathWithoutPlaceSelected() {
-        onView(withText("Draw Path")).perform(click());
-    }
 
-    /**
-     * Test for canceling the get places operation.
-     * Ensures that canceling the operation results in the expected behavior.
-     *
-     * @throws InterruptedException if the test is interrupted
-     */
-    @Test
-    public void testCancel() {
-        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
-        // Find the menu item by its ID and perform a click
-        onView(withText("Get Place")).perform(click());
-        onView(withId(R.id.edit_text_radius2)).perform(typeText("1500"), closeSoftKeyboard());
-        onView(withText("Show Places")).inRoot(isDialog()).perform(click());
-        onView(withText("Cancel")).perform(click());
-    }
+    }}
 
-    /**
-     * Test for clicking on a specific location.
-     * Ensures that clicking on a location results in the expected behavior.
-     *
-     * @throws InterruptedException if the test is interrupted
-     */
-    @Test
-    public void testClickOnLocation() {
-        String placeName = "GooglePlex Football";
-        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
-        // Find the menu item by its ID and perform a click
-        onView(withText("Get Place")).perform(click());
-        onView(withId(R.id.edit_text_radius2)).perform(typeText("1500"), closeSoftKeyboard());
-        onView(withText("Show Places")).inRoot(isDialog()).perform(click());
-        onView(withText(placeName)).perform(click());
-    }
-
-
-    /**
-     * Test for the case when no radius is input.
-     * Ensures that the application handles the situation when no radius is provided.
-     *
-     * @throws InterruptedException if the test is interrupted
-     */
-
-
-    /**
-     * Test for the case when there are no places within the specified radius.
-     * Ensures that the application handles the situation when no places are found within the specified radius.
-     *
-     * @throws InterruptedException if the test is interrupted
-     */
-    @Test
-    public void testIfNoPlacesWithinRadius() {
-        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
-        // Find the menu item by its ID and perform a click
-        onView(withText("Get Place")).perform(click());
-        onView(withId(R.id.edit_text_radius2)).perform(typeText("100"), closeSoftKeyboard());
-        onView(withText("Show Places")).inRoot(isDialog()).perform(click());
-    }
-
-}
