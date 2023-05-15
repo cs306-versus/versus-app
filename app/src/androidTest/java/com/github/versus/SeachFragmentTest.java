4f02d2ffb70d397844ce464faea86119e19d3f61
Adam-Github0 <MernissiAdam1l@gmail.com>
2023-05-15 23:13:31 +0200

    Implement search bar

--- app/src/androidTest/java/com/github/versus/SeachFragmentTest.java
+++ app/src/androidTest/java/com/github/versus/SeachFragmentTest.java
@@ -63,7 +63,7 @@ public class SeachFragmentTest {
         onView(withId(android.R.id.button1)).perform(click());
         onView(withId(android.R.id.button1)).perform(click());
     }
-
+*/
     @Test
     public void testCancelPost(){
         onView(withId(R.id.add_posts)).perform(click());
@@ -75,7 +75,7 @@ public class SeachFragmentTest {
         onView(withId(R.id.editMaxPlayers)).perform(typeText("4"), closeSoftKeyboard());
         onView(withId(android.R.id.button2)).perform(click());
     }
-*/
+
     //@Test
     //public void testSearchBar(){
     //    onView(withId(R.id.search_posts)).perform(typeText("Football"), closeSoftKeyboard());
