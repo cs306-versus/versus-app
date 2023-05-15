64009ea340f981f463154f962e53552a022e53d0
unknown <aymane.lamyaghri@epfl.ch>
2023-05-15 08:05:00 +0200

    Test

---
+++ app/src/androidTest/java/com/github/versus/ZZSportsEquipmentsFragmentTest.java
@@ -0,0 +1,97 @@
+package com.github.versus;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import static java.util.Objects.requireNonNull;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.is;
+
+
+import androidx.core.view.GravityCompat;
+
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.DrawerMatchers;
+
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ZZSportsEquipmentsFragmentTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
+
+    /**
+     * Switch the view to the Schedule
+     */
+    @Before public void testScheduleButtons2(){
+
+        onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isClosed(GravityCompat.START))).perform(DrawerActions.open());
+        onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen(GravityCompat.START)));
+        onView(withId(R.id.nav_sport_equipments)).perform(click());
+    }
+
+    @Test
+    public void testing(){
+
+        ViewInteraction rectangleView1 = onView(withId(R.id.the_real_right_arrow));
+        rectangleView1.perform(ViewActions.scrollTo());
+        onView(withId(R.id.the_real_right_arrow)).perform(click());
+        onView(withId(R.id.the_real_right_arrow)).perform(click());
+        onView(withId(R.id.the_real_right_arrow)).perform(click());
+        onView(withId(R.id.the_real_left_arrow)).perform(click());
+        onView(withId(R.id.the_real_left_arrow)).perform(click());
+        onView(withId(R.id.the_real_left_arrow)).perform(click());
+        ViewInteraction rectangleView2 = onView(withId(R.id.contact_person));
+        rectangleView2.perform(ViewActions.scrollTo());
+        try {
+            Thread.sleep(2000); // wait for 10 seconds
+        } catch (InterruptedException e) {
+            // handle the exception
+        }
+        onView(withId(R.id.contact_person)).perform(click());
+        ViewInteraction rectangleView3 = onView(withId(R.id.back_to_posts_container));
+        rectangleView3.perform(ViewActions.scrollTo());
+        try {
+            Thread.sleep(2000); // wait for 10 seconds
+        } catch (InterruptedException e) {
+            // handle the exception
+        }
+        onView(withId(R.id.back_to_posts_container)).perform(click());
+        onView(withId(R.id.search_for_post)).perform(typeText("Boxing"), closeSoftKeyboard());
+
+
+        try {
+            Thread.sleep(2000); // wait for 10 seconds
+        } catch (InterruptedException e) {
+            // handle the exception
+        }
+
+
+    }
+
+
+}
+
