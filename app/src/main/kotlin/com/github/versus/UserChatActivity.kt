a65a8002c5134632836d0ff7dbdb132bddf95921
@DerouichAbdessalam <abdessalam.derouich@epfl.ch>
2023-05-18 16:49:52 +0200

    messages  written update last pos

--- app/src/main/kotlin/com/github/versus/UserChatActivity.kt
+++ app/src/main/kotlin/com/github/versus/UserChatActivity.kt
@@ -1,28 +1,22 @@
 package com.github.versus;
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.util.Log
-import android.view.MenuItem
-import android.view.WindowManager
-import android.widget.EditText;
+import android.graphics.Rect
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.ViewTreeObserver
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.fasterxml.jackson.databind.ObjectMapper
+import androidx.recyclerview.widget.RecyclerView
 import com.github.versus.chats.Chat
 import com.github.versus.chats.Message
 import com.github.versus.chats.MessageAdapter
-import com.github.versus.db.FsChatManager
 import com.github.versus.posts.Timestamp
 import com.github.versus.user.DummyUser
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import java.time.Month
 
 class UserChatActivity : AppCompatActivity(){
@@ -104,13 +98,30 @@ class UserChatActivity : AppCompatActivity(){
             messageList.add(message)
             messageAdapter.notifyDataSetChanged()
             messageBox.text = ""
+            val itemCount: Int = messageAdapter.itemCount
+            chatRecyclerView.smoothScrollToPosition(itemCount - 1)
         }
         backButton.setOnClickListener(){
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
+        val decorView = window.decorView
+        decorView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            private val rect = Rect()
 
+            override fun onGlobalLayout() {
+                decorView.getWindowVisibleDisplayFrame(rect)
+                val screenHeight = decorView.height
+                val keypadHeight = screenHeight - rect.bottom
+
+                if (keypadHeight > screenHeight * 0.15) {
+                    // Keyboard is visible, scroll to the last item
+                    val lastItemPosition = messageAdapter.itemCount - 1
+                    chatRecyclerView.smoothScrollToPosition(lastItemPosition)
+                }
+            }
+        })
     }
 
 }
