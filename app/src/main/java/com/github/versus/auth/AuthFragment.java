7794e76ba9ad2822360822c5e93d0f76e294e285
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-05-18 23:30:59 +0200

    Add logic of AuthFragment + bring back SignInFragment

--- app/src/main/java/com/github/versus/auth/AuthFragment.java
+++ app/src/main/java/com/github/versus/auth/AuthFragment.java
@@ -2,7 +2,11 @@ package com.github.versus.auth;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +14,6 @@ import android.view.ViewGroup;
 
 import com.github.versus.R;
 import com.github.versus.databinding.FragmentAuthBinding;
-import com.github.versus.databinding.FragmentSignInBinding;
 
 public class AuthFragment extends Fragment {
 
@@ -18,9 +21,21 @@ public class AuthFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAuthBinding.inflate(inflater);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        binding.register.setOnClickListener(v -> switchTo(RegisterFragment.class));
+        binding.signIn.setOnClickListener(v -> switchTo(SignInFragment.class));
+    }
+
+    private void switchTo(Class<? extends Fragment> clz){
+        FragmentManager manager = getParentFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragmentContainerView, clz, null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/github/versus/auth/SignInFragment.java
@@ -0,0 +1,91 @@
+package com.github.versus.auth;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.github.versus.R;
+import com.github.versus.databinding.SigninFragmentBinding;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+
+/**
+ * ???
+ */
+public class SignInFragment extends BaseAuthFragment {
+
+    private SigninFragmentBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = SigninFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.authSignin.setOnClickListener(this::signInRequest);
+        registerLoginButton(binding.authLoginMail);
+        binding.authLoginGoogle.setOnClickListener(this::loginWithGoogleRequest);
+    }
+
+    /**
+     * ???
+     *
+     * @param view
+     */
+    private void loginWithGoogleRequest(View view) {
+        switchTo(GoogleAuthFragment.class);
+    }
+
+    /**
+     * ???
+     *
+     * @param view
+     */
+    private void signInRequest(View view) {
+        switchTo(SignInFragment.class);
+    }
+
+    private void switchTo(Class<? extends Fragment> clz){
+        FragmentManager manager = getParentFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragmentContainerView, clz, null);
+        transaction.commit();
+    }
+
+    @Override
+    protected Task<AuthResult> requestAuthentication() {
+        String mailText = binding.authLoginMailMail.getText().toString();
+        String pwdText = binding.authLoginMailPwd.getText().toString();
+        return auth.signInWithMail(mailText, pwdText);
+    }
+
+    @Override
+    protected void handleSuccessfulConnection(AuthResult result) {
+
+    }
+
+    @Override
+    protected void handleFailedConnection(Exception exception) {
+        Toast.makeText(getContext(), "handleFailedConnection",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void handleCancelledConnection() {
+        Toast.makeText(getContext(), "handleCancelledConnection",
+                Toast.LENGTH_SHORT).show();
+    }
+}
