c9c4310702daa06f2aaf066b5c25c5b399efc6c7
Adam-Github0 <MernissiAdam1l@gmail.com>
2023-05-19 04:02:41 +0200

    Tests

--- app/src/androidTest/java/com/github/versus/BarSearchTest.java
+++ app/src/androidTest/java/com/github/versus/BarSearchTest.java
@@ -102,9 +102,41 @@ public class BarSearchTest {
 
         IdlingRegistry.getInstance().unregister(idlingResource2);
 
+    }
+    @Test
+    public void testSearchBarByClicking() throws InterruptedException {
+        // Open the options menu in the action bar
+        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+
+        // Find the menu item "Search" by its text and perform a click. This should open the search bar.
+        onView(withText("Search")).perform(click());
+
+        long waitingTime = 6000;
+        // Get the instance of the device on which the test is running
+        ElapsedTimeIdlingResource idlingResourceFirst = new ElapsedTimeIdlingResource(waitingTime);
+
+        onView(withId(R.id.autocomplete_location_search)).perform(click());
+
+
+        IdlingRegistry.getInstance().register(idlingResourceFirst);
+
+        IdlingRegistry.getInstance().unregister(idlingResourceFirst);
 
 
     }
+
+
+    /**
+     * Simulates a map click at the given screen coordinates.
+     *
+     * @param x int representing the x-coordinate of the screen location to click
+     * @param y int representing the y-coordinate of the screen location to click
+     */
+    private void simulateMapClick(int x, int y) {
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        device.click(x, y);
+    }
+
     /**
      * ElapsedTimeIdlingResource is an implementation of the IdlingResource interface.
      * It is used to wait for a specified amount of time before allowing the test to continue.
