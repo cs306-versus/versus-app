3919c557047e71a53282a25a1704b9387b8f45f6
@DerouichAbdessalam <abdessalam.derouich@epfl.ch>
2023-05-19 19:23:11 +0200

    connec the list of users to the database

--- app/src/main/kotlin/com/github/versus/ChatsFragment.kt
+++ app/src/main/kotlin/com/github/versus/ChatsFragment.kt
@@ -7,11 +7,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.versus.chats.Chat
+import com.github.versus.db.FsChatManager
+import com.github.versus.db.FsUserManager
+import com.github.versus.user.User
 import com.github.versus.user.UserAdapter
 import com.github.versus.user.VersusUser
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.concurrent.CompletableFuture
 
 class ChatsFragment : Fragment() {
 
@@ -40,32 +45,24 @@ class ChatsFragment : Fragment() {
 
         userRecyclerView.adapter = adapter
 
-        userList.add(VersusUser.Builder("asdfghjklö").setFirstName("Abdess").setLastName("Derouich").build())
-        userList.add(VersusUser.Builder("qwertzuiop").setFirstName("Aymane").setLastName("Lamyaghri").build())
-        userList.add(VersusUser.Builder("stevzdbpg ").setFirstName("Adam").setLastName("Mernissi").build())
-        userList.add(VersusUser.Builder("hehehehe").setFirstName("De Bruyne").setLastName("hamada").build())
-        userList.add(VersusUser.Builder("nibbisbvfd").setFirstName("Gustavo").setLastName("Peperoni").build())
+        // getting the current user from the database
 
-        /* code to use once complete link to user database is made
-        // Add a listener to the "user" collection
-        mDbRef.addSnapshotListener { snapshot: QuerySnapshot?, error: FirebaseFirestoreException? ->
-            if (error != null) {
-                // Handle errors here
-                return@addSnapshotListener
+        val currUserUID = VersusUser.computeUID("jane.doe@versus.ch")
+        val fman = FsUserManager(FirebaseFirestore.getInstance())
+        val future = fman.fetch(currUserUID) as CompletableFuture<VersusUser>
+        future.thenAccept { currUser ->
+
+            val friendsUID = currUser.friends
+            friendsUID.forEach { uid ->
+                val userFuture = fman.fetch(uid) as CompletableFuture<VersusUser>
+                userFuture.thenAccept{
+                    friend ->
+                        userList.add(friend)
+                }
             }
-
-            // Clear the userList before adding new data
-            userList.clear()
-
-            // Loop through each document in the "user" collection
-            for (document in snapshot?.documents!!) {
-                // Convert the document to a VersusUser object and add it to the userList
-                val currentUser = document.toObject(VersusUser::class.java)
-                userList.add(currentUser!!)
-            }
-            adapter.notifyDataSetChanged()
         }
-        */
+
+
 
         return view
     }
