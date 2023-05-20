457a7e32dd262b02a333af9e9d2ad2750e8c3ebd
Adam-Github0 <MernissiAdam1l@gmail.com>
2023-05-20 15:56:37 +0200

    Fix UI of dialog boxes

--- app/src/main/java/com/github/versus/announcements/ChoosePostSportDialogFragment.java
+++ app/src/main/java/com/github/versus/announcements/ChoosePostSportDialogFragment.java
@@ -3,13 +3,23 @@ package com.github.versus.announcements;
 import static android.app.AlertDialog.*;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
@@ -32,8 +42,123 @@ public class ChoosePostSportDialogFragment extends DialogFragment {
 
     SportListener tl;
     Sport sport = Sport.SOCCER;
-
     @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Activity a = getActivity();
+        Fragment f = getParentFragment();
+        tl = (SportListener) f;
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        // Inflate custom layout
+        View dialogView = inflater.inflate(R.layout.custom_dialog_layout_list, null);
+
+        // Get RadioGroup from the custom layout and set the items
+        RadioGroup radioGroup = dialogView.findViewById(R.id.radioGroup);
+        final String[] items = Stream.of(Sport.values()).map(sport -> sport.name).collect(Collectors.toList()).toArray(new String[0]);
+
+        // Add RadioButton for each sport
+        // Add RadioButton for each sport
+        for (int i = 0; i < items.length; i++) {
+            RadioButton radioButton = new RadioButton(a);
+            radioButton.setText(items[i]);
+            radioButton.setId(i);
+            radioButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+            radioButton.setTextColor(Color.WHITE);
+
+            // Set text size
+            int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, a.getResources().getDisplayMetrics()); // Convert 10dp to pixels
+            radioButton.setPadding(padding, padding, padding, padding); // Set padding
+            radioGroup.addView(radioButton);
+        }
+        // When a RadioButton is clicked
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> sport = Sport.values()[checkedId]);
+
+        // Use the Builder class for convenient dialog construction
+        Builder builder = new Builder(a);
+        builder.setView(dialogView);
+
+
+
+
+        builder.setPositiveButton("Next", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        tl.onSportPositiveClick(sport);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        tl.onCancel();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface arg0) {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.WHITE);
+                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.WHITE);
+            }
+        });
+
+
+        return dialog;
+    }
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            Window window = dialog.getWindow();
+            if (window != null) {
+                // Apply rounded corners
+                window.setBackgroundDrawableResource(R.drawable.custom_dialog_background);
+
+                // Set size
+                WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+                layoutParams.copyFrom(window.getAttributes());
+                layoutParams.width = 50; // Set width to your preference
+                layoutParams.height = 600; // Set height to your preference
+                window.setAttributes(layoutParams);
+            }
+        }
+    }*/
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            Window window = dialog.getWindow();
+            if (window != null) {
+                // Apply rounded corners
+                window.setBackgroundDrawableResource(R.drawable.custom_dialog_background);
+
+                // Post a runnable to resize the dialog
+                window.getDecorView().post(() -> {
+                    // Get current screen size
+                    DisplayMetrics displayMetrics = new DisplayMetrics();
+                    getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+                    // Define how much width and height you want to set
+                    int dialogWindowWidth = (int) (displayMetrics.widthPixels * 0.85); // 85% of screen width
+                    int dialogWindowHeight = (int) (displayMetrics.heightPixels * 0.30); // 30% of screen height
+
+                    // Set size
+                    WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+                    layoutParams.copyFrom(window.getAttributes());
+                    layoutParams.width = dialogWindowWidth;
+                    layoutParams.height = dialogWindowHeight;
+                    window.setAttributes(layoutParams);
+                });
+            }
+        }
+    }
+
+
+
+
+    /*@Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         Activity a = getActivity();
@@ -43,7 +168,7 @@ public class ChoosePostSportDialogFragment extends DialogFragment {
         Builder builder = new Builder(a);
         FsPostManager fpm = new FsPostManager(FirebaseFirestore.getInstance());
         final String[] items = Stream.of(Sport.values()).map(sport -> sport.name).collect(Collectors.toList()).toArray(new String[0]);
-        builder.setTitle("choose a sport").setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener(){
+        builder.setTitle("Choose a sport").setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         sport = Sport.values()[i];
@@ -54,14 +179,14 @@ public class ChoosePostSportDialogFragment extends DialogFragment {
                         tl.onSportPositiveClick(sport);
                     }
                 })
-                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         tl.onCancel();
                     }
                 });
         // Create the AlertDialog object and return it
         return builder.create();
-    }
+    }*/
 
     @Override
     public void onAttach(Context context){
--- app/src/main/java/com/github/versus/announcements/CreatePostTitleDialogFragment.java
+++ app/src/main/java/com/github/versus/announcements/CreatePostTitleDialogFragment.java
@@ -3,12 +3,18 @@ package com.github.versus.announcements;
 import static android.app.AlertDialog.*;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 import androidx.fragment.app.DialogFragment;
@@ -21,27 +27,30 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class CreatePostTitleDialogFragment extends DialogFragment {
 
     public interface TitleListener extends CancelCreate {
-        public void onTitlePositiveClick(String title);
+        void onTitlePositiveClick(String title);
     }
 
     TitleListener tl;
 
-
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
         Activity a = getActivity();
         Fragment f = getParentFragment();
         tl = (TitleListener) f;
         LayoutInflater inflater = requireActivity().getLayoutInflater();
-        //Adam
-        View innerView = inflater.inflate(R.layout.create_post_layout, null);
-        Builder builder = new Builder(a);
+
+        // Inflate custom layout
+        View innerView = inflater.inflate(R.layout.custom_dialog_post_layout, null);
+
+        // Find your EditText view
+        EditText et = innerView.findViewById(R.id.editPostTitle);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(a, R.style.CustomAlertDialog);
+
         FsPostManager fpm = new FsPostManager(FirebaseFirestore.getInstance());
-        builder.setMessage("create a post").setView(innerView)
+        builder.setView(innerView)
                 .setPositiveButton("Next", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        EditText et = innerView.findViewById(R.id.editPostTitle);
                         String title = et.getText().toString();
                         tl.onTitlePositiveClick(title);
                     }
@@ -51,14 +60,56 @@ public class CreatePostTitleDialogFragment extends DialogFragment {
                         tl.onCancel();
                     }
                 });
+
         // Create the AlertDialog object and return it
-        return builder.create();
+        AlertDialog dialog = builder.create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface arg0) {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.WHITE);
+                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.WHITE);
+            }
+        });
+
+
+        return dialog;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            Window window = dialog.getWindow();
+            if (window != null) {
+                // Apply rounded corners
+                window.setBackgroundDrawableResource(R.drawable.custom_dialog_background);
+
+                // Post a runnable to resize the dialog
+                window.getDecorView().post(() -> {
+                    // Get current screen size
+                    DisplayMetrics displayMetrics = new DisplayMetrics();
+                    getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+                    // Define how much width and height you want to set
+                    int dialogWindowWidth = (int) (displayMetrics.widthPixels * 0.85); // 85% of screen width
+                    int dialogWindowHeight = (int) (displayMetrics.heightPixels * 0.25); // 85% of screen height
+
+                    // Set size
+                    WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+                    layoutParams.copyFrom(window.getAttributes());
+                    layoutParams.width = dialogWindowWidth;
+                    layoutParams.height = dialogWindowHeight;
+                    window.setAttributes(layoutParams);
+                });
+            }
+        }
+    }
+
 
     @Override
     public void onAttach(Context context){
         super.onAttach(context);
-//        tl = (TitleListener) context;
     }
-
 }
