95bcb1a7a2b2f89f8d1a36e3b6f694a9815f9c0e
Adam-Github0 <MernissiAdam1l@gmail.com>
2023-05-21 12:48:57 +0200

    Fix UI of dialog boxes

--- app/src/main/java/com/github/versus/announcements/PostDatePickerDialog.java
+++ app/src/main/java/com/github/versus/announcements/PostDatePickerDialog.java
@@ -46,39 +46,11 @@ public class PostDatePickerDialog extends DialogFragment implements DatePickerDi
     }
 
 
+
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
         Timestamp ts = new Timestamp(year, Month.of(month), day, 4, 4, Timestamp.Meridiem.PM);
         listener.onPickPostDate(ts);
     }
 }
-/*public class PostDatePickerDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-    public interface PickDateListener extends CancelCreate {
-        public void onPickPostDate(Timestamp ts);
-    }
-
-    PickDateListener listener;
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Fragment f = getParentFragment();
-        listener = (PickDateListener) f;
-        final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
-        // Create a new instance of DatePickerDialog and return it
-        DatePickerDialog d = new DatePickerDialog(requireContext(), this, year, month, day);
-        //        d.getButton(DatePickerDialog.BUTTON_POSITIVE).setText("next");
-        d.setMessage("When will you meet?");
-        return d;
-    }
-
-
-    @Override
-    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-        Timestamp ts = new Timestamp(year, Month.of(month), day, 4, 4, Timestamp.Meridiem.PM);
-        listener.onPickPostDate(ts);
-    }
-}*/
