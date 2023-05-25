14f9c17a4d2c9f11eda03ff96fff1fb72f26928b
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-05-25 20:40:31 +0200

    Add logic

--- app/src/main/java/com/github/versus/auth/RegisterFragment.java
+++ app/src/main/java/com/github/versus/auth/RegisterFragment.java
@@ -59,7 +59,7 @@ public final class RegisterFragment extends Fragment {
         String firstName = binding.firstName.getText().toString();
         String lastName = binding.lastName.getText().toString();
         if (!pwd.equals(pwd_confirmation)) {
-            binding.pwd.getBackground().setState(new int[]{R.attr.pwd_state});
+            binding.passwordsAreNotSimilar.setVisibility(View.VISIBLE);
         }
         VersusUser.VersusBuilder builder = new VersusUser.VersusBuilder(null);
         builder.setFirstName(firstName)
@@ -84,7 +84,8 @@ public final class RegisterFragment extends Fragment {
         });
         // HR : if the connection failed
         task.addOnFailureListener(ex -> {
-            Log.d("TAG", "account creation failed");
+            binding.mailAlreadyUsed.setVisibility(View.VISIBLE);
+            Log.d("TAG", ex.toString());
         });
         // HR : if the connection was cancelled
         task.addOnCanceledListener(() -> {
