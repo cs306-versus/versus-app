aeaee15c2025065cf00445a96fb39f31c69bc4fc
Hamza Remmal <hamzaremmalpriv@gmail.com>
2023-05-25 22:32:50 +0200

    second draft of the ScheduleTest

--- app/src/test/java/com/github/versus/ScheduleTest.java
+++ app/src/test/java/com/github/versus/ScheduleTest.java
@@ -1,14 +1,22 @@
 package com.github.versus;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import com.github.versus.schedule.Schedule;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public final class ScheduleTest {
+
+    // ============================================================================================
+    // ================================ CONSTRUCTORS TESTS ========================================
+    // ============================================================================================
 
     @Test
     public void testDefaultConstructor(){
@@ -20,10 +28,51 @@ public final class ScheduleTest {
         assertNotNull(new Schedule("123456789"));
     }
 
+    // ============================================================================================
+    // ========================================== getUID TESTS ====================================
+    // ============================================================================================
+
     @Test
     public void testGetUID(){
         String uid = "123456789";
         assertEquals(uid, new Schedule(uid).getUID());
+    }
+
+    // ============================================================================================
+    // ==================================== GET POSTS TEST ========================================
+    // ============================================================================================
+
+    @Test
+    public void testGetPost(){
+        assertTrue(new Schedule().getPosts().isEmpty());
+    }
+
+    // ============================================================================================
+    // ====================================== test getAllAttributes ===============================
+    // ============================================================================================
+
+    @Test
+    public void testGetAllAttributes(){
+        assertFalse(new Schedule().getAllAttributes().isEmpty());
+    }
+
+    //
+    //
+    // ============================================================================================
+
+    @Test
+    public void testAddPost(){
+        new Schedule().addPost(null);
+    }
+
+    @Test
+    public void testAddPosts(){
+        new Schedule().addPosts(new ArrayList<>());
+    }
+
+    @Test
+    public void testRemovePost(){
+        new Schedule().removePost("uid");
     }
 
     // ============================================================================================
