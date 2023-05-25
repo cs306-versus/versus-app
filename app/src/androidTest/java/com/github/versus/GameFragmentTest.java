0ca1b1de9231ffef293947d46fe3da0b4d189552
unknown <aymane.lamyaghri@epfl.ch>
2023-05-25 16:51:23 +0200

    Try fix test

--- app/src/androidTest/java/com/github/versus/GameFragmentTest.java
+++ app/src/androidTest/java/com/github/versus/GameFragmentTest.java
@@ -3,11 +3,14 @@ package com.github.versus;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -22,6 +25,8 @@ import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.DrawerMatchers;
 
 
+import com.github.versus.auth.AuthActivity;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -33,7 +38,7 @@ import org.junit.runner.RunWith;
 public class GameFragmentTest {
     //General rule that sets up the Activity Scenario Rule
     @Rule
-    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<AuthActivity> scenario = new ActivityScenarioRule<>(AuthActivity.class);
 
     //Initiating the intents
     @Before
@@ -53,52 +58,61 @@ public class GameFragmentTest {
      */
     @Test
     public void testGameFragment() {
+        try {
+            Thread.sleep(5000); // wait for 10 seconds
+        } catch (InterruptedException e) {
+            // handle the exception
+        }
+        onView(withId(R.id.signin_btn)).perform(click());
+        try {
+            Thread.sleep(5000); // wait for 10 seconds
+        } catch (InterruptedException e) {
+            // handle the exception
+        }
+      onView(ViewMatchers.withId(R.id.mail))
+                .perform(typeText("john.doe@versus.ch"), closeSoftKeyboard());
+
+        // Enter password
+        onView(ViewMatchers.withId(R.id.pwd))
+                .perform(typeText("123456789"), closeSoftKeyboard());
+        try {
+            Thread.sleep(5000); // wait for 10 seconds
+        } catch (InterruptedException e) {
+            // handle the exception
+        }
+
+        // Click the Sign In button
+        onView(ViewMatchers.withId(R.id.signin))
+                .perform(click());
+        try {
+            Thread.sleep(10000); // wait for 10 seconds
+        } catch (InterruptedException e) {
+            // handle the exception
+        }
+        onView(withId(R.id.arrow_11)).perform(click());
         //Opening the drawer
         onView(withId(R.id.main_activity_layout)).check(matches(DrawerMatchers.isClosed(GravityCompat.START))).perform(DrawerActions.open());
         onView(withId(R.id.main_activity_layout)).check(matches(DrawerMatchers.isOpen(GravityCompat.START)));
         //Clicking on the  nav_schedule button
         onView(withId(R.id.nav_schedule)).perform(click());
 
-        try {
-            Thread.sleep(10000); // wait for 1 second
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         //Getting back two weeks before where there is a game in the database
         onView(withId(R.id.arrow_11)).perform(click());
         onView(withId(R.id.arrow_11)).perform(click());
         onView(withId(R.id.arrow_11)).perform(click());
         onView(withId(R.id.arrow_11)).perform(click());
         onView(withId(R.id.Tuesday_button)).perform(click());
-        try {
-            Thread.sleep(10000); // wait for 1 second
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         //Scrolling down so that  the first item of the list of games is visible
         ViewInteraction rectangleView1 = onView(withId(R.id.rectangle1));
         rectangleView1.perform(ViewActions.scrollTo());
         rectangleView1.perform(ViewActions.scrollTo());
         rectangleView1.perform(ViewActions.scrollTo());
         rectangleView1.perform(ViewActions.scrollTo());
-        try {
-            Thread.sleep(10000); // wait for 1 second
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
-            Thread.sleep(10000); // wait for 1 second
-        } catch (InterruptedException e) {
-            e.printStackTrace();
 
 
-        }
         //Performing the click on the first item of the games in the schedule
         onData(anything()).inAdapterView(withId(R.id.list_view)).atPosition(0).perform(click());
-        try {
-            Thread.sleep(7000); // wait for 1 second
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
     }
 }
