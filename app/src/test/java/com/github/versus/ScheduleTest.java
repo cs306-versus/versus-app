e1944e8d1efac04bab40d79f5341ed830413f39b
ziazi <mehdi.ziazi@epfl.ch>
2023-05-26 04:20:59 +0200

    Adding new tests

--- app/src/test/java/com/github/versus/ScheduleTest.java
+++ app/src/test/java/com/github/versus/ScheduleTest.java
@@ -6,11 +6,22 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import com.github.versus.offline.SimpleTestPost;
+import com.github.versus.posts.Location;
+import com.github.versus.posts.Post;
+import com.github.versus.posts.Timestamp;
 import com.github.versus.schedule.Schedule;
+import com.github.versus.sports.Sport;
 
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
 
 public final class ScheduleTest {
 
@@ -96,6 +107,145 @@ public final class ScheduleTest {
         Schedule schedule1 = new Schedule(uid);
         Schedule schedule2 = new Schedule(uid);
         assertEquals(schedule1, schedule2);
+    }
+
+
+    @Test
+    public void startingFromDateYieldsCorrectPosts(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Timestamp ts2= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 1, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Post post1= SimpleTestPost.postWith("Valid",
+                ts1, new Location("Lausanne",10,10),10, Sport.SOCCER);
+        Post post2=  SimpleTestPost.postWith("Another Valid one",
+                ts2, new Location("Lausanne",10,10),10, Sport.SOCCER);
+
+        schedule.addPosts(List.of(post1,post2));
+        Schedule result= schedule.startingFromDate(ts1);
+        Assert.assertTrue(result.getPosts().contains(post1)&& !result.getPosts().contains(post2));
+
+    }
+
+    @Test
+    public void startingFromDateYieldsCorrectUid(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Timestamp ts2= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 1, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Post post1= SimpleTestPost.postWith("Valid",
+                ts1, new Location("Lausanne",10,10),10, Sport.SOCCER);
+        Post post2=  SimpleTestPost.postWith("Another Valid one",
+                ts2, new Location("Lausanne",10,10),10, Sport.SOCCER);
+
+        schedule.addPosts(List.of(post1,post2));
+        Schedule result= schedule.startingFromDate(ts1);
+        assertEquals(result.getUID(), uid);
+
+    }
+
+    @Test
+    public void OnDateYieldsCorrectPosts(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Timestamp ts2= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 1, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Post post1= SimpleTestPost.postWith("Valid",
+                ts1, new Location("Lausanne",10,10),10, Sport.SOCCER);
+        Post post2=  SimpleTestPost.postWith("Another Valid one",
+                ts2, new Location("Lausanne",10,10),10, Sport.SOCCER);
+
+        schedule.addPosts(List.of(post1,post2));
+        Schedule result= schedule.onDate(ts1);
+        Assert.assertTrue(result.getPosts().contains(post1)&& !result.getPosts().contains(post2));
+
+    }
+
+    @Test
+    public void OnDateYieldsCorrectUid(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Timestamp ts2= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 1, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Post post1= SimpleTestPost.postWith("Valid",
+                ts1, new Location("Lausanne",10,10),10, Sport.SOCCER);
+        Post post2=  SimpleTestPost.postWith("Another Valid one",
+                ts2, new Location("Lausanne",10,10),10, Sport.SOCCER);
+
+        schedule.addPosts(List.of(post1,post2));
+        Schedule result= schedule.onDate(ts1);
+        assertEquals(result.getUID(), uid);
+
+    }
+
+    @Test
+    public void getAllAttributesYieldCorrectOutput(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Post post1= SimpleTestPost.postWith("Valid",
+                ts1, new Location("Lausanne",10,10),10, Sport.SOCCER);
+
+        schedule.addPost(post1);
+        Map<String,Object> result= schedule.getAllAttributes();
+        Map<String,Object> expected=  Map.of("UID",uid,"posts", List.of(post1));
+        Assert.assertEquals(expected,result);
+    }
+
+    @Test
+    public void equalsWithSelfWorks(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Assert.assertEquals(schedule,schedule);
+    }
+
+    @Test
+    public void equalsWithWrongScheduleFails(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Assert.assertNotEquals(schedule,new Schedule());
+    }
+
+    @Test
+    public void equalsWithCloseScheduleFails(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Post post1= SimpleTestPost.postWith("Valid",
+                ts1, new Location("Lausanne",10,10),10, Sport.SOCCER);
+
+        schedule.addPost(post1);
+        Assert.assertNotEquals(schedule,new Schedule(uid));
+    }
+
+    @Test
+    public void equalsWithSameAttributesWorks(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Post post1= SimpleTestPost.postWith("Valid",
+                ts1, new Location("Lausanne",10,10),10, Sport.SOCCER);
+
+        schedule.addPost(post1);
+
+        Schedule similar= new Schedule(uid);
+        similar.addPost(post1);
+        Assert.assertEquals(schedule,similar);
+
+    }
+
+    @Test
+    public void equalsWithSWrongObjectFails(){
+        String uid = "123456789";
+        Timestamp ts1= new Timestamp(Calendar.getInstance().get(Calendar.YEAR), Month.JANUARY, 10, 8, 1, Timestamp.Meridiem.PM);
+        Schedule schedule = new Schedule(uid);
+        Post post1= SimpleTestPost.postWith("Valid",
+                ts1, new Location("Lausanne",10,10),10, Sport.SOCCER);
+        schedule.addPost(post1);
+        Assert.assertNotEquals(schedule,new Object());
+
     }
 
 }
