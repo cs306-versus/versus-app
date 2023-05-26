55e6bfc3142333429dcb9709e9e88e09c73200e6
Hamza Remmal <56235032+hamzaremmal@users.noreply.github.com>
2023-05-26 04:55:52 +0000

    Update ChatTest.java

--- app/src/androidTest/java/com/github/versus/ChatTest.java
+++ app/src/androidTest/java/com/github/versus/ChatTest.java
@@ -1,11 +1,13 @@
-
-
 package com.github.versus;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import static com.github.versus.utils.EmulatorUserProvider.validMail;
+import static com.github.versus.utils.EmulatorUserProvider.validPassword;
 
 import androidx.core.view.GravityCompat;
 import androidx.test.espresso.contrib.DrawerActions;
@@ -15,7 +17,12 @@ import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.github.versus.auth.VersusAuthenticator;
+import com.github.versus.db.FsUserManager;
 import com.github.versus.utils.FirebaseEmulator;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.junit.After;
@@ -24,30 +31,51 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.ExecutionException;
 
-//@RunWith(AndroidJUnit4.class)
+
+@RunWith(AndroidJUnit4.class)
 public final class ChatTest {
 
-    static {
-        FirebaseFirestore db = FirebaseEmulator.FIREBASE_FIRESTORE;
-    }
 
     @Rule
     public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
 
-    /**
-     * This method tests the first position of the trending sports by clicking on the appropriate picture and
-     * showing the filtered posts of the sport
-     */
-    //@Test
-    public void testOnFirstPosition() {
-        //Opening the drawer menu
+    @Before
+    public void setup() throws ExecutionException, InterruptedException {
+        // Sign in the user
+        VersusAuthenticator auth = VersusAuthenticator.getInstance(FirebaseAuth.getInstance());
+        Task<AuthResult> task = auth.signInWithMail(validMail(), validPassword());
+        spinAndWait(task);
+        FsUserManager fsm = new FsUserManager(FirebaseFirestore.getInstance());
+        fsm.addFriend(FirebaseAuth.getInstance().getUid(), "0ZnBDoch6feHhmNxlLjPNSne0HL2").get();
+
+
         onView(withId(R.id.main_activity_layout)).check(matches(DrawerMatchers.isClosed(GravityCompat.START))).perform(DrawerActions.open());
         onView(withId(R.id.main_activity_layout)).check(matches(DrawerMatchers.isOpen(GravityCompat.START)));
         //Performing the click on the trending sports button
         onView(withId(R.id.nav_chats)).perform(click());
-        // Click on the first item in the RecyclerView
-        //onView(withId(R.id.usersRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+    }
+    
+    @Test
+    public void testChatActivity() throws ExecutionException, InterruptedException {
+        onView(withId(R.id.usersRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.inputMessage)).perform(click());
+        onView(withId(R.id.inputMessage)).perform(replaceText("haha"));
+        onView(withId(R.id.imageSend)).perform(click());
+        FsUserManager fsm = new FsUserManager(FirebaseFirestore.getInstance());
+
+        fsm.unfriendAll(FirebaseAuth.getInstance().getUid());
+        //
+
+    }
+
+
+    private Task<AuthResult> spinAndWait(Task<AuthResult> task){
+        // spin and wait for the task to complete
+        while (!(task.isComplete() || task.isCanceled())) ;
+        return task;
     }
 
 }
