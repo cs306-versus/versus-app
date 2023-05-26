414fd110c6579ca85de0fa9eedf00e18a31be788
unknown <aymane.lamyaghri@epfl.ch>
2023-05-26 07:18:48 +0200

    Test

--- app/src/androidTest/java/com/github/versus/GameFragmentTest.java
+++ app/src/androidTest/java/com/github/versus/GameFragmentTest.java
@@ -3,17 +3,12 @@ package com.github.versus;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
@@ -28,7 +23,6 @@ import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.DrawerMatchers;
 
 
-import com.github.versus.auth.AuthActivity;
 import com.github.versus.auth.VersusAuthenticator;
 import com.github.versus.utils.FirebaseEmulator;
 import com.google.android.gms.tasks.Task;
@@ -39,10 +33,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 
-@RunWith(AndroidJUnit4.class)
+//@RunWith(AndroidJUnit4.class)
 public class GameFragmentTest {
 
     static {
