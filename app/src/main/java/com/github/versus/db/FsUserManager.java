a5b41cbb0138a21e9fa4a2283cc79d7b377ef15f
@DerouichAbdessalam <abdessalam.derouich@epfl.ch>
2023-05-26 05:26:48 +0200

    bvdi

--- app/src/main/java/com/github/versus/db/FsUserManager.java
+++ app/src/main/java/com/github/versus/db/FsUserManager.java
@@ -112,7 +112,7 @@ public class FsUserManager implements DataBaseManager<User> {
                 .setUserName(doc.get(USERNAME_FIELD, String.class))
                 .setMail(doc.get(MAIL_FIELD, String.class))
                 .setPhone(doc.get(PHONE_FIELD, String.class))
-                .setRating(doc.get(RATING_FIELD) == null ? Rating.DEFAULT_ELO : ((Long)doc.get(RATING_FIELD)).intValue())
+                .setRating(Rating.DEFAULT_ELO)
                 .setPreferredSports((List<Sport>)doc.get(PREF_SPORTS_FIELD))
                 .setCity(doc.get(CITY_FIELD, String.class))
                 .setFriends((List<String>)doc.get(FRIENDS_FIELD));
