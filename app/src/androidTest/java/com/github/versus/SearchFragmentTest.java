e06d4fe9d648ff761dd3d76d64d9525c7d9c5139
Adam-Github0 <MernissiAdam1l@gmail.com>
2023-05-26 06:52:24 +0200

    Fix coverage

--- app/src/androidTest/java/com/github/versus/SearchFragmentTest.java
+++ app/src/androidTest/java/com/github/versus/SearchFragmentTest.java
@@ -89,6 +89,7 @@ public final class SearchFragmentTest {
 
     @Test
   public void testCreatePostWithLocation() throws InterruptedException {
+
         onView(withId(R.id.add_posts)).perform(click());
         onView(withId(R.id.editPostTitle)).check(matches(isDisplayed()));
         onView(withId(R.id.editPostTitle)).perform(typeText("TEST POST"), closeSoftKeyboard());
